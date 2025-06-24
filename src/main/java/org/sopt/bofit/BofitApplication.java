bcd6f90c3db82bdb1c701bda097ddc1a8197e0ea
dlwjddus1112 <dlwjddus1112@naver.com>
2025-06-24 18:28:22 +0900

    first commit

---
+++ src/main/java/org/sopt/bofit/BofitApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.bofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BofitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/bofit/BofitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.bofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
