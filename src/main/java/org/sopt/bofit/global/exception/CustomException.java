8a9def8c1eb7270dfda1caf571edb871ca203633
dlwjddus1112 <dlwjddus1112@naver.com>
2025-06-26 15:45:04 +0900

    [Mod] #1 CustomException 추상 클래스로 변경

--- src/main/java/org/sopt/bofit/global/exception/CustomException.java
+++ src/main/java/org/sopt/bofit/global/exception/CustomException.java
@@ -5,18 +5,18 @@ import org.sopt.bofit.global.exception.constant.ErrorCode;
 
 
 @Getter
-public class CustomException extends RuntimeException {
+public abstract class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
     private final String message;
     private final String messageDetail;
 
-    public CustomException(ErrorCode errorCode) {
+    protected CustomException(ErrorCode errorCode) {
         this.errorCode = errorCode;
         this.message = errorCode.getMessage();
         this.messageDetail = null;
     }
 
-    public CustomException(ErrorCode errorCode, String messageDetail){
+    protected CustomException(ErrorCode errorCode, String messageDetail) {
         this.errorCode = errorCode;
         this.message = errorCode.getMessage();
         this.messageDetail = messageDetail;
