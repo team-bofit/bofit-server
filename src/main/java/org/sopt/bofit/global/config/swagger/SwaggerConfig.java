3011a93899360bce583edc5d32d985ae84583917
dlwjddus1112 <dlwjddus1112@naver.com>
2025-06-28 20:15:08 +0900

    [Feat] #3 SwaggerConfig에 응답 예시 명세 및 Security 관련 항목 추가

--- src/main/java/org/sopt/bofit/global/config/swagger/SwaggerConfig.java
+++ src/main/java/org/sopt/bofit/global/config/swagger/SwaggerConfig.java
@@ -4,8 +4,30 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.sopt.bofit.global.annotation.CustomExceptionDescription;
+import org.sopt.bofit.global.exception.constant.ErrorCode;
+import org.sopt.bofit.global.exception.constant.GlobalErrorCode;
+import org.sopt.bofit.global.response.BaseErrorResponse;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
 
 
 @OpenAPIDefinition(
@@ -18,9 +40,112 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+        private final String securitySchemaName = "JWT";
+
         @Bean
-        public OpenAPI openAPI() {
+        public OpenAPI customOpenAPI() {
                 return new OpenAPI()
-                        .components(new Components());
+                        .components(setComponents())
+                        .addSecurityItem(setSecurityItems());
+        }
+        private Components setComponents() {
+                return new Components()
+                        .addSecuritySchemes(securitySchemaName, bearerAuth());
+        }
+
+        private SecurityScheme bearerAuth() {
+                return new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name(HttpHeaders.AUTHORIZATION);
+        }
+
+        private SecurityRequirement setSecurityItems() {
+                return new SecurityRequirement()
+                        .addList(securitySchemaName);
+        }
+
+        @Bean
+        public OperationCustomizer customize() {
+                return (Operation operation, HandlerMethod handlerMethod) -> {
+
+                        CustomExceptionDescription customExceptionDescription = handlerMethod.getMethodAnnotation(
+                                CustomExceptionDescription.class);
+
+                        addGlobalErrorResponses(operation);
+                        // CustomExceptionDescription 어노테이션 단 메소드 적용
+                        if (customExceptionDescription != null) {
+                                generateErrorCodeResponseExample(operation, customExceptionDescription.value());
+                        }
+
+                        return operation;
+                };
+        }
+
+        private void generateErrorCodeResponseExample(
+                Operation operation, SwaggerResponseDescription type) {
+
+                ApiResponses responses = operation.getResponses();
+
+                Set<ErrorCode> errorCodeList = type.getErrorCodeList();
+
+                Map<Integer, List<ExampleHolder>> statusWithExampleHolders =
+                        errorCodeList.stream()
+                                .map(
+                                        errorCode -> {
+                                                return ExampleHolder.builder()
+                                                        .holder(
+                                                                getSwaggerExample(errorCode))
+                                                        .code(errorCode.getHttpStatus())
+                                                        .name(errorCode.toString())
+                                                        .build();
+                                        }
+                                ).collect(groupingBy(ExampleHolder::getCode));
+                addExamplesToResponses(responses, statusWithExampleHolders);
+        }
+
+        private Example getSwaggerExample(ErrorCode errorCode) {
+                BaseErrorResponse errorResponse = BaseErrorResponse.of(errorCode);
+                Example example = new Example();
+                example.description(errorCode.getMessage());
+                example.setValue(errorResponse);
+                return example;
+        }
+
+        private void addExamplesToResponses(
+                ApiResponses responses, Map<Integer, List<ExampleHolder>> statusWithExampleHolders) {
+                statusWithExampleHolders.forEach(
+                        (status, v) -> {
+                                Content content = new Content();
+                                MediaType mediaType = new MediaType();
+                                ApiResponse apiResponse = new ApiResponse();
+                                v.forEach(
+                                        exampleHolder -> {
+                                                mediaType.addExamples(
+                                                        exampleHolder.getName(), exampleHolder.getHolder());
+                                        });
+                                content.addMediaType("application/json", mediaType);
+                                apiResponse.setDescription("");
+                                apiResponse.setContent(content);
+                                responses.addApiResponse(status.toString(), apiResponse);
+                        });
+        }
+
+        private void addGlobalErrorResponses(Operation operation) {
+                ApiResponses responses = operation.getResponses();
+
+                for (GlobalErrorCode errorCode : GlobalErrorCode.values()) {
+                        Example example = getSwaggerExample(errorCode);
+                        MediaType mediaType = new MediaType().addExamples(errorCode.name(), example);
+                        Content content = new Content().addMediaType("application/json", mediaType);
+
+                        ApiResponse apiResponse = new ApiResponse()
+                                .description(errorCode.getMessage())
+                                .content(content);
+
+                        responses.addApiResponse(String.valueOf(errorCode.getHttpStatus()), apiResponse);
+                }
         }
 }
--- src/main/java/org/sopt/bofit/global/config/swagger/SwaggerResponseDescription.java
+++ src/main/java/org/sopt/bofit/global/config/swagger/SwaggerResponseDescription.java
@@ -1,10 +1,12 @@
 package org.sopt.bofit.global.config.swagger;
 
+import lombok.Getter;
 import org.sopt.bofit.global.exception.constant.ErrorCode;
 
 import java.util.Set;
 
 
+@Getter
 public enum SwaggerResponseDescription {
     ;
     private final Set<ErrorCode> errorCodeList;
