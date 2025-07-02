1519cb7153bc23a1e2fa7e677570008974799bf4
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-02 17:09:01 +0900

    [Feat] #5 보장 우선순위 관련 JSON Converter 클래스 구현

---
+++ src/main/java/org/sopt/bofit/global/converter/CoveragePreferenceMapConverter.java
@@ -0,0 +1,37 @@
+package org.sopt.bofit.global.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.sopt.bofit.domain.user.entity.constant.ConveragePreference;
+import org.sopt.bofit.global.exception.constant.GlobalErrorCode;
+import org.sopt.bofit.global.exception.custom_exception.JsonConvertException;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Converter
+public class CoveragePreferenceMapConverter implements AttributeConverter<Map<ConveragePreference,Integer>,String> {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<ConveragePreference, Integer> attribute) {
+        try {
+            return mapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new JsonConvertException(GlobalErrorCode.JSON_SERIALIZATION_ERROR);
+        }
+    }
+
+    @Override
+    public Map<ConveragePreference, Integer> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, new TypeReference<LinkedHashMap<ConveragePreference, Integer>>() {});
+        } catch (IOException e) {
+            throw new JsonConvertException(GlobalErrorCode.JSON_DESERIALIZATION_ERROR);
+        }
+    }
+}
