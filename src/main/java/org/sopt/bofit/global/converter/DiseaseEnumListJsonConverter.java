c1954656b43445a3c25903b6210c520034885538
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-02 16:47:25 +0900

    [Feat] 질병 Enum 관련 JSON Converter 구현

---
+++ src/main/java/org/sopt/bofit/global/converter/DiseaseEnumListJsonConverter.java
@@ -0,0 +1,36 @@
+package org.sopt.bofit.global.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
+import org.sopt.bofit.global.exception.constant.GlobalErrorCode;
+import org.sopt.bofit.global.exception.custom_exception.JsonConvertException;
+
+import java.io.IOException;
+import java.util.List;
+
+@Converter
+public class DiseaseEnumListJsonConverter implements AttributeConverter<List<DiagnosedDisease>, String> {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<DiagnosedDisease> attribute) {
+        try {
+            return mapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new JsonConvertException(GlobalErrorCode.JSON_SERIALIZATION_ERROR);
+        }
+    }
+
+    @Override
+    public List<DiagnosedDisease> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new JsonConvertException(GlobalErrorCode.JSON_DESERIALIZATION_ERROR);
+        }
+    }
+}
