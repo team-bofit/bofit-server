614c56b01c8c916268dfeceb34b7d2d2adfd241e
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-04 20:32:36 +0900

    [Chore] #12 Spring Security 의존성 추가
    # Conflicts:
    #	build.gradle

---
+++ src/main/java/org/sopt/bofit/global/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package org.sopt.bofit.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
