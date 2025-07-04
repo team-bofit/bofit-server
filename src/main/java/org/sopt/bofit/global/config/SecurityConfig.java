0b8ae582be5f8a77386a42ccc49f2cd35796946b
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-04 20:39:45 +0900

    [Feat] #12 Security 설정 파일 추가

--- src/main/java/org/sopt/bofit/global/config/SecurityConfig.java
+++ src/main/java/org/sopt/bofit/global/config/SecurityConfig.java
@@ -2,7 +2,32 @@ package org.sopt.bofit.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] ALLOWED_PATHS = {
+            "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
+            "/oauth/kakao/login"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers(ALLOWED_PATHS).permitAll()
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll() // 개발을 위해 일시적으로 허용
+                );
+
+        return http.build();
+    }
 }
