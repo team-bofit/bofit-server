bf27af6683c0494e23be23764900c41eadc3957b
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-04 22:49:34 +0900

    [Chore] #12 User에서 이메일 필드 삭제

--- src/main/java/org/sopt/bofit/domain/user/entity/User.java
+++ src/main/java/org/sopt/bofit/domain/user/entity/User.java
@@ -32,9 +32,6 @@ public class User extends BaseEntity {
 
     private String nickname;
 
-    @Column(unique = true)
-    private String email;
-
     @Column(name = "profile_image")
     private String profileImage;
 
