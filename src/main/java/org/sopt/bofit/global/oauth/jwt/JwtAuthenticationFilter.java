8659b7307c15f19bce6acd174d6410d17ec2af09
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-05 18:18:40 +0900

    [Feat] #12 JWT 커스텀 필터 구현

---
+++ src/main/java/org/sopt/bofit/global/oauth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package org.sopt.bofit.global.oauth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.bofit.global.exception.custom_exception.CustomException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getToken(request);
+
+        if(token != null) {
+            try {
+                if (jwtUtil.isTokenValid(token)) {
+                    Authentication authentication = getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (CustomException e) {
+                log.debug("토큰 검증 실패: {}", e.getMessage());
+                request.setAttribute("exception", e.getErrorCode());
+                throw e;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return uri.startsWith("/swagger-ui") || uri.startsWith("/v3/api-docs") || uri.startsWith("/swagger-config");
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authorization = request.getHeader("authorization");
+        String validTokenPrefix = "Bearer ";
+        if (authorization == null || !authorization.startsWith(validTokenPrefix)) {
+            return null;
+        }
+        return authorization.substring(validTokenPrefix.length()).trim();
+    }
+
+    private Authentication getAuthentication(String token) {
+        Long userId = jwtUtil.extractUserIdFromToken(token);
+        return new JwtTokenAuthentication(userId);
+    }
+
+}
