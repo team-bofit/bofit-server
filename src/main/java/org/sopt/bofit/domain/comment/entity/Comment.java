d6959f954aa765fb1f9d3664e4ea186a5577965f
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-06 21:37:25 +0900

    [Feat] #9 Comment 엔티티 생성

---
+++ src/main/java/org/sopt/bofit/domain/comment/entity/Comment.java
@@ -0,0 +1,36 @@
+package org.sopt.bofit.domain.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.user.entity.User;
+import org.sopt.bofit.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CommentStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/org/sopt/bofit/domain/comment/entity/CommentStatus.java
@@ -0,0 +1,8 @@
+package org.sopt.bofit.domain.comment.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum CommentStatus {
+    ACTIVE, INACTIVE
+}
