b80c9d136562b7ab3d9f66b8f28ad10ab74b9132
jher235 <tim668666@gmail.com>
2025-07-06 18:59:01 +0900

    [Feat] #14 Insurance 상품 일당 입원비 작성

---
+++ src/main/java/org/sopt/bofit/domain/insurance/entity/DailyHospitalization.java
@@ -0,0 +1,17 @@
+package org.sopt.bofit.domain.insurance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class DailyHospitalization {
+	@Column(name = "daily_hospitalization_disease", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer disease;
+
+	@Column(name = "daily_hospitalization_injury", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer injury;
+}
