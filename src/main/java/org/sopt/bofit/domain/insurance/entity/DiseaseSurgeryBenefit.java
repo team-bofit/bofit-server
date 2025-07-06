0979c19952d7f9ce9d9ef2b746c14e2027729ca0
jher235 <tim668666@gmail.com>
2025-07-06 18:58:41 +0900

    [Feat] #14 Insurance 상품 질병 수술비 작성

---
+++ src/main/java/org/sopt/bofit/domain/insurance/entity/DiseaseSurgeryBenefit.java
@@ -0,0 +1,30 @@
+package org.sopt.bofit.domain.insurance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class DiseaseSurgeryBenefit {
+	@Column(name = "disease_surgery", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer general;
+
+	@Column(name = "disease_surgery_type1", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer type1;
+	@Column(name = "disease_surgery_type2", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer type2;
+	@Column(name = "disease_surgery_type3", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer type3;
+	@Column(name = "disease_surgery_type4", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer type4;
+	@Column(name = "disease_surgery_type5", nullable = false,
+		columnDefinition = "SMALLINT UNSIGNED DEFAULT 0")
+	private Integer type5;
+
+}
