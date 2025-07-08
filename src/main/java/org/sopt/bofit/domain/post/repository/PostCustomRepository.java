f120a94d806820aceb198eb53935f10c1de52395
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 00:10:56 +0900

    [Feat] #22 QueryDSL Repository 생성

--- src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepository.java
+++ src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepository.java
@@ -1,4 +1,9 @@
 package org.sopt.bofit.domain.post.repository;
 
+import org.sopt.bofit.domain.post.entity.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
 public interface PostCustomRepository {
+    Slice<Post> findMyPosts(Long userId, Pageable pageable);
 }
--- src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepositoryImpl.java
@@ -1,17 +1,29 @@
 package org.sopt.bofit.domain.post.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.post.entity.constant.PostStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static org.sopt.bofit.domain.post.entity.QPost.post;
+
+
 @Repository
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PostCustomRepositoryImpl implements PostCustomRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    
+    @Override
+    public Slice<Post> findMyPosts(Long userId, Pageable pageable) {
 
+        return new SliceImpl<>();
+    }
 }
 
