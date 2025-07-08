75d9a97f8852b6a2e0f00f0e3c7268f9ee15c184
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 01:07:45 +0900

    [Feat] #22 내 글 조회 Service 로직 작성

--- src/main/java/org/sopt/bofit/domain/post/service/PostService.java
+++ src/main/java/org/sopt/bofit/domain/post/service/PostService.java
@@ -1,7 +1,12 @@
 package org.sopt.bofit.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.post.repository.PostCustomRepositoryImpl;
 import org.sopt.bofit.domain.post.repository.PostRepository;
+import org.sopt.bofit.domain.user.dto.response.MyPostsResponse;
+import org.sopt.bofit.domain.user.dto.response.SliceResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +14,11 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+
+    private final PostCustomRepositoryImpl postCustomRepositoryImpl;
+
+    public SliceResponse<MyPostsResponse> getMyPosts(Long userId, Pageable pageable) {
+        Slice<MyPostsResponse> posts = postCustomRepositoryImpl.findMyPosts(userId, pageable);
+        return SliceResponse.of(posts);
+    }
 }
