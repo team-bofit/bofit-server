6562d82710c4ac2fe7c00cb8eb33ac648822b3ca
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 01:05:38 +0900

    [Feat] #22 QueryDSL 이용한 내 글 응답 DTO 생성

--- src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepository.java
+++ src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepository.java
@@ -1,9 +1,10 @@
 package org.sopt.bofit.domain.post.repository;
 
 import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.user.dto.response.MyPostsResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface PostCustomRepository {
-    Slice<Post> findMyPosts(Long userId, Pageable pageable);
+    Slice<MyPostsResponse> findMyPosts(Long userId, Pageable pageable);
 }
--- src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/org/sopt/bofit/domain/post/repository/PostCustomRepositoryImpl.java
@@ -1,9 +1,14 @@
 package org.sopt.bofit.domain.post.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.comment.entity.CommentStatus;
+import org.sopt.bofit.domain.comment.entity.QComment;
 import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.post.entity.QPost;
 import org.sopt.bofit.domain.post.entity.constant.PostStatus;
+import org.sopt.bofit.domain.user.dto.response.MyPostsResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -21,9 +26,33 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Slice<Post> findMyPosts(Long userId, Pageable pageable) {
+    public Slice<MyPostsResponse> findMyPosts(Long userId, Pageable pageable) {
 
-        return new SliceImpl<>();
+        QPost post = QPost.post;
+        QComment comment = QComment.comment;
+
+        List<MyPostsResponse> content = queryFactory
+                .select(Projections.constructor(MyPostsResponse.class,
+                        post.id,
+                        post.title,
+                        post.content,
+                        comment.id.count().intValue(),
+                        post.createdAt
+                ))
+                .from(post)
+                .leftJoin(comment).on(comment.post.eq(post), comment.status.eq(CommentStatus.ACTIVE))
+                .where(post.user.id.eq(userId),
+                        post.status.eq(PostStatus.ACTIVE))
+                .groupBy(post.id)
+                .orderBy(post.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        boolean hasNext = content.size() > pageable.getPageSize();
+        if (hasNext) content.remove(pageable.getPageSize());
+
+        return new SliceImpl<>(content, pageable, hasNext);
     }
 }
 
