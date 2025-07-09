86f6dd1242c75d2ed3d45fa668d9346bb94f42ad
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 23:23:18 +0900

    [Feat] #32 게시글 수정 서비스 로직 구현

--- src/main/java/org/sopt/bofit/domain/post/service/PostService.java
+++ src/main/java/org/sopt/bofit/domain/post/service/PostService.java
@@ -7,9 +7,12 @@ import org.sopt.bofit.domain.post.repository.PostRepository;
 import org.sopt.bofit.domain.user.entity.User;
 import org.sopt.bofit.domain.user.repository.UserRepository;
 import org.sopt.bofit.domain.user.service.UserReadService;
+import org.sopt.bofit.global.exception.constant.PostErrorCode;
 import org.sopt.bofit.global.exception.custom_exception.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.sopt.bofit.global.exception.constant.PostErrorCode.*;
 import static org.sopt.bofit.global.exception.constant.UserErrorCode.*;
 
 @Service
@@ -28,5 +31,15 @@ public class PostService {
         postRepository.save(newPost);
         return PostResponse.from(newPost.getId());
     }
+
+    @Transactional
+    public PostResponse updatePost (Long userId, Long postId, String title, String content) {
+        User user = userReadService.findUserById(userId);
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NotFoundException(POST_NOT_FOUND));
+        
+        post.updatePost(title, content);
+        return PostResponse.from(post.getId());
+    }
+
 
 }
--- src/main/java/org/sopt/bofit/global/exception/constant/PostErrorCode.java
+++ src/main/java/org/sopt/bofit/global/exception/constant/PostErrorCode.java
@@ -8,7 +8,8 @@ public enum PostErrorCode implements ErrorCode {
     POST_TITLE_BLANK(HttpStatus.BAD_REQUEST.value(), "제목은 비어있을 수 없습니다."),
     POST_CONTENT_BLANK(HttpStatus.BAD_REQUEST.value(), "본문은 비어있을 수 없습니다."),
     POST_TITLE_LONG(HttpStatus.BAD_REQUEST.value(), "제목은 30자 미만으로 입력해주세요."),
-    POST_CONTENT_LONG(HttpStatus.BAD_REQUEST.value(), "내용은 3000자 미만으로 작성해주세요")
+    POST_CONTENT_LONG(HttpStatus.BAD_REQUEST.value(), "내용은 3000자 미만으로 작성해주세요"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 게시글입니다."),
 
     ;
     private final int httpStatus;
