7e80cd3eb04569aed1d7fdd03066bd1440f7dbea
jher235 <tim668666@gmail.com>
2025-07-10 01:04:47 +0900

    [Feat] #26 이름 관련 정규표현식 추가

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/constant/InsuranceReportConstant.java
@@ -0,0 +1,11 @@
+package org.sopt.bofit.domain.insurancereport.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class InsuranceReportConstant {
+
+	public final static String NAME_REGEX = "^[가-힣]+$";
+
+}
