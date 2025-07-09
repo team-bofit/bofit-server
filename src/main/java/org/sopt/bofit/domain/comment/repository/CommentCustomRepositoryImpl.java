984c6585489701508b11774931ffeffba61f4ef0
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 15:24:34 +0900

    [Feat] #28 내 댓글 조회 로직 구현

--- src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepositoryImpl.java
+++ src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepositoryImpl.java
@@ -1,11 +1,48 @@
 package org.sopt.bofit.domain.comment.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.comment.entity.CommentStatus;
 import org.sopt.bofit.domain.user.dto.response.MyCommentsResponse;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+import org.sopt.bofit.domain.comment.entity.QComment;
 
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class CommentCustomRepositoryImpl implements CommentCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
     @Override
     public Slice<MyCommentsResponse> findCommentsByCursorId(Long userId, Long cursorId, int size) {
-        return null;
+        QComment comment = QComment.comment;
+
+        List<MyCommentsResponse> content = queryFactory
+                .select(Projections.constructor(MyCommentsResponse.class,
+                        comment.id,
+                        comment.content,
+                        comment.createdAt
+                ))
+                .from(comment)
+                .where(
+                        comment.user.id.eq(userId),
+                        comment.status.eq(CommentStatus.ACTIVE),
+                        cursorId != null ? comment.id.lt(cursorId) : null
+                )
+                .groupBy(comment.id)
+                .orderBy(comment.id.desc())
+                .limit(size + 1)
+                .fetch();
+
+        boolean hasNext = content.size() > size;
+        if (hasNext) content.remove(size);
+
+        return new SliceImpl<>(content, PageRequest.of(0, size), hasNext);
     }
 }
--- src/main/java/org/sopt/bofit/domain/user/dto/response/MyCommentsResponse.java
+++ src/main/java/org/sopt/bofit/domain/user/dto/response/MyCommentsResponse.java
@@ -6,13 +6,16 @@ import java.time.LocalDateTime;
 
 public record MyCommentsResponse(
 
+        @Schema(description = "댓글 ID")
+        Long commentId,
+
         @Schema(description = "댓글 내용", example = "저도요")
         String content,
 
         @Schema(description = "작성 시간")
         LocalDateTime createdAt
 ) {
-    public static MyCommentsResponse of(String content, LocalDateTime createdAt) {
-        return new MyCommentsResponse(content, createdAt);
+    public static MyCommentsResponse of(Long commentId, String content, LocalDateTime createdAt) {
+        return new MyCommentsResponse(commentId, content, createdAt);
     }
 }
