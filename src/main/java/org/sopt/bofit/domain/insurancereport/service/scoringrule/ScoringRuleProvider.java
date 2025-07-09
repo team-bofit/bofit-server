560f0ad6fc040ee533b86e747d07b036b6293793
jher235 <tim668666@gmail.com>
2025-07-10 01:09:08 +0900

    [Feat] #26 스코어링 룰 조회 담당 ScoringRuleProvider 작성

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/ScoringRuleProvider.java
@@ -0,0 +1,32 @@
+package org.sopt.bofit.domain.insurancereport.service.scoringrule;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.diseasehistory.DiseaseHistoryScoringRule;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.userinfo.UserInfoRuleType;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.userinfo.UserInfoScoringRule;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.DiseaseHistoryScoringRuleRepository;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.FamilyHistoryScoringRuleRepository;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.SelectedScoringRuleRepository;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.UserInfoScoringRuleRepository;
+import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ScoringRuleProvider {
+	private final UserInfoScoringRuleRepository userInfoScoringRuleRepository;
+	private final DiseaseHistoryScoringRuleRepository diseaseHistoryScoringRuleRepository;
+	private final FamilyHistoryScoringRuleRepository familyHistoryScoringRuleRepository;
+	private final SelectedScoringRuleRepository selectedScoringRuleRepository;
+
+	public List<UserInfoScoringRule> findAll(UserInfoRuleType userInfoRuleType){
+		return userInfoScoringRuleRepository.findAllByUserInfoRuleType(userInfoRuleType);
+	}
+
+
+
+}
