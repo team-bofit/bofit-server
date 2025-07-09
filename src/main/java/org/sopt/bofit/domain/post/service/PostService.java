37295d61f4214d64ddba937c648610790b2e6466
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 18:58:06 +0900

    [Feat] #30 게시물 작성 비즈니스 로직 작성

--- src/main/java/org/sopt/bofit/domain/post/service/PostService.java
+++ src/main/java/org/sopt/bofit/domain/post/service/PostService.java
@@ -1,8 +1,17 @@
 package org.sopt.bofit.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.post.dto.request.PostRequest;
+import org.sopt.bofit.domain.post.dto.response.PostResponse;
+import org.sopt.bofit.domain.post.entity.Post;
 import org.sopt.bofit.domain.post.repository.PostRepository;
+import org.sopt.bofit.domain.user.entity.User;
+import org.sopt.bofit.domain.user.repository.UserRepository;
+import org.sopt.bofit.global.exception.constant.UserErrorCode;
+import org.sopt.bofit.global.exception.custom_exception.NotFoundException;
 import org.springframework.stereotype.Service;
+
+import static org.sopt.bofit.global.exception.constant.UserErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +19,15 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    private final UserRepository userRepository;
+
+    public PostResponse createPost(Long userId, String title, String content) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+        Post newPost = Post.create(title, content);
+        newPost.setUser(user);
+
+        postRepository.save(newPost);
+        return PostResponse.of(newPost.getId());
+    }
 
 }
