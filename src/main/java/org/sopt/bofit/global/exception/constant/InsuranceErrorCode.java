b4208b6f4139679ef4b36469f53943b4aa92fe4a
jher235 <tim668666@gmail.com>
2025-07-10 03:26:24 +0900

    [Feat] #26 유저 선택 보장 순위가 잘못된 경우의 에러 코드 추가

--- src/main/java/org/sopt/bofit/global/exception/constant/InsuranceErrorCode.java
+++ src/main/java/org/sopt/bofit/global/exception/constant/InsuranceErrorCode.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum InsuranceErrorCode implements ErrorCode{
 
+	INVALID_COVERAGE_SELECTED_NUMBER(HttpStatus.BAD_REQUEST.value(), "잘못된 우선 순위 요구사항입니다."),
 	NOT_FOUND_INSURANCE_TOTAL_AVERAGE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "전 연령 보험 보장 평균 데이터를 찾을 수 없습니다.")
 	;
 
