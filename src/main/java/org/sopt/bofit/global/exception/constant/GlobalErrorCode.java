baea200c1cb51fa9caaadd968aa31c820ad3066a
jher235 <tim668666@gmail.com>
2025-07-09 18:02:12 +0900

    [Feat] #26 에러코드 추가

--- src/main/java/org/sopt/bofit/global/exception/constant/GlobalErrorCode.java
+++ src/main/java/org/sopt/bofit/global/exception/constant/GlobalErrorCode.java
@@ -7,6 +7,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @AllArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 요청입니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "유효하지 않은 Http 메서드입니다."),
     NOT_FOUND_PATH(NOT_FOUND.value(), "존재하지 않는 API 입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류입니다."),
