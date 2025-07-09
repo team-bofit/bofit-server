afff543d325a8f2c37d7b1000142de4018d34a8b
jher235 <tim668666@gmail.com>
2025-07-10 02:37:40 +0900

    [Feat] #26 질병력 가중치 계산

--- src/main/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/ScoringRuleCalculator.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/ScoringRuleCalculator.java
@@ -2,7 +2,9 @@ package org.sopt.bofit.domain.insurancereport.service.scoringrule;
 
 import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
 import org.sopt.bofit.domain.insurancereport.entity.scoringrule.InsuranceScoringRule;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.diseasehistory.DiseaseHistoryScoringRule;
 import org.sopt.bofit.domain.user.entity.User;
+import org.sopt.bofit.domain.user.entity.UserInfo;
 import org.sopt.bofit.global.exception.constant.GlobalErrorCode;
 import org.sopt.bofit.global.exception.custom_exception.InternalException;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,18 @@ public class ScoringRuleCalculator {
 	private final ScoringRuleProvider scoringRuleProvider;
 
 	private final UserInfoRuleCalculator userInfoRuleCalculator;
+	private final DiseaseHistoryRuleCalculator diseaseHistoryRuleCalculator;
 
+	public double calculatorScoringRule(User user, UserInfo userInfo, InsuranceProduct product){
+		return 0;
+	}
 
 	public double calculateUserInfoRule(User user, InsuranceProduct product){
 		return userInfoRuleCalculator.calculate(user, product);
+	}
+
+	public double calculateDiseaseHistoryRule(UserInfo userInfo, InsuranceProduct product){
+		return diseaseHistoryRuleCalculator.calculate(userInfo.getDiseasesHistory(), product);
 	}
 
 }
