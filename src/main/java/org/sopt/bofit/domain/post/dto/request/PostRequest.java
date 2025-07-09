5d712975ff85d565411d952554ed6b0fdb92c72d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-09 20:13:41 +0900

    [Refactor] #30 제목, 내용 max 값 상수로 분리

--- src/main/java/org/sopt/bofit/domain/post/dto/request/PostRequest.java
+++ src/main/java/org/sopt/bofit/domain/post/dto/request/PostRequest.java
@@ -4,16 +4,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Length;
+import org.sopt.bofit.domain.post.entity.constant.PostInfoConstant;
 
 public record PostRequest(
         @Schema(description = "글 제목", example = "아니")
         @NotBlank(message = "제목은 비어있을 수 없습니다.")
-        @Length(max = 30)
+        @Length(max = PostInfoConstant.MAX_TITLE_LENGTH)
         String title,
 
         @Schema(description = "내용", example = "저희 대상타면 어떡하나요 ㅈㅉ로?")
         @NotBlank(message = "본문은 비어있을 수 없습니다.")
-        @Length(max = 3000, message = "내용은 3000자 미만으로 작성해주세요.")
+        @Length(max = PostInfoConstant.MAX_CONTENT_LENGTH, message = "내용은 {max}자 미만으로 작성해주세요.")
         String content
 ) {
 }
