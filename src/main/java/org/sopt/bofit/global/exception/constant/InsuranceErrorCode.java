e50474e8e0b2b1e3eeba18cb87d6f264d3e14f5a
jher235 <tim668666@gmail.com>
2025-07-10 00:58:23 +0900

    [Feat] #26 보험 관련 예외 코드 작성

---
+++ src/main/java/org/sopt/bofit/global/exception/constant/InsuranceErrorCode.java
@@ -0,0 +1,25 @@
+package org.sopt.bofit.global.exception.constant;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum InsuranceErrorCode implements ErrorCode{
+
+	NOT_FOUND_INSURANCE_TOTAL_AVERAGE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "전 연령 보험 보장 평균 데이터를 찾을 수 없습니다.")
+	;
+
+	private final int httpStatus;
+	private final String message;
+
+	@Override
+	public int getHttpStatus() {
+		return httpStatus;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
