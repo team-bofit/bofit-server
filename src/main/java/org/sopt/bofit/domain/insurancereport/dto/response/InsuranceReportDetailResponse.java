3762e94ff2d1d02f5ba3366fb7f063b450b9243f
jher235 <tim668666@gmail.com>
2025-07-11 04:26:53 +0900

    [Feat] #26 보험 리포트 상세 조회 dto 구현

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/InsuranceReportDetailResponse.java
@@ -0,0 +1,32 @@
+package org.sopt.bofit.domain.insurancereport.dto.response;
+
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurance.entity.statistic.InsuranceStatistic;
+import org.sopt.bofit.domain.insurancereport.dto.response.dailyHospitalization.DailyHospitalizationTotalSection;
+import org.sopt.bofit.domain.insurancereport.dto.response.death.DeathSection;
+import org.sopt.bofit.domain.insurancereport.dto.response.death.DeathTotalSection;
+import org.sopt.bofit.domain.insurancereport.dto.response.disability.DisabilitySection;
+import org.sopt.bofit.domain.insurancereport.dto.response.disability.DisabilityTotalSection;
+import org.sopt.bofit.domain.insurancereport.dto.response.majordisease.MajorDiseaseTotalSection;
+import org.sopt.bofit.domain.insurancereport.dto.response.surgery.SurgeryTotalSection;
+import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
+
+public record InsuranceReportDetailResponse (
+	MajorDiseaseTotalSection majorDiseaseTotalSection,
+	SurgeryTotalSection surgeryTotalSection,
+	DailyHospitalizationTotalSection dailyHospitalizationTotalSection,
+	DisabilityTotalSection disabilityTotalSection,
+	DeathTotalSection deathTotalSection
+) {
+
+	public static InsuranceReportDetailResponse of(InsuranceReport report, InsuranceProduct product, InsuranceStatistic average){
+		return new InsuranceReportDetailResponse(
+			MajorDiseaseTotalSection.of(report, product, average),
+			SurgeryTotalSection.of(report, product, average),
+			DailyHospitalizationTotalSection.of(report, product, average),
+			DisabilityTotalSection.of(report, product, average),
+			DeathTotalSection.of(report, product, average)
+		);
+	}
+
+}
