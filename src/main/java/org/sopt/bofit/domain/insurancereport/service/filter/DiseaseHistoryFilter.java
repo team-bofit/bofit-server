f5ccf4b9869231ca3ffaf1e4cbd99e5b64eeb65d
jher235 <tim668666@gmail.com>
2025-07-10 13:00:30 +0900

    [Feat] #26 질병 관련 사항들을 필터링

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/service/filter/DiseaseHistoryFilter.java
@@ -0,0 +1,54 @@
+package org.sopt.bofit.domain.insurancereport.service.filter;
+
+import java.util.List;
+
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.user.entity.UserInfo;
+import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DiseaseHistoryFilter {
+	public List<InsuranceProduct> filtering(List<InsuranceProduct> products, UserInfo userInfo){
+		return products.stream()
+			.filter(product -> isCoveredDiseaseHistory(userInfo, product))
+			.toList();
+	}
+
+	private boolean isCoveredDiseaseHistory(UserInfo userInfo, InsuranceProduct product) {
+		List<DiagnosedDisease> diseaseHistory = userInfo.getDiseaseHistory();
+
+		return diseaseHistory.stream().allMatch(disease -> isDiseaseCovered(disease, product));
+	}
+
+	private boolean isDiseaseCovered(DiagnosedDisease disease, InsuranceProduct product) {
+		return switch (disease) {
+			case CANCER -> isCancerCovered(product);
+			case CEREBROVASCULAR -> isCerebrovascularCovered(product);
+			case HEART -> isHeartCovered(product);
+			case RESPIRATORY, RIVER, KIDNEY -> isSurgeryCovered(product);
+			default -> true;
+		};
+	}
+
+	private boolean isCancerCovered(InsuranceProduct product) {
+		var cancer = product.getMajorDisease().getCancer();
+		return cancer.getGeneralSurgery() > 0 && cancer.getGeneralDiagnosis() > 0;
+	}
+
+	private boolean isCerebrovascularCovered(InsuranceProduct product) {
+		var cerebrovascular = product.getMajorDisease().getCerebrovascular();
+		return cerebrovascular.getHemorrhageDiagnosis() > 0 && cerebrovascular.getHemorrhageSurgery() > 0;
+	}
+
+	private boolean isHeartCovered(InsuranceProduct product) {
+		var heart = product.getMajorDisease().getHeart();
+		return heart.getIschemicDiagnosis() > 0 && heart.getIschemicSurgery() > 0;
+	}
+
+	private boolean isSurgeryCovered(InsuranceProduct product) {
+		var surgery = product.getSurgery();
+		return surgery.getDisease() > 0 || surgery.getDiseaseType5() > 0;
+	}
+
+}
