1c2e9f593cae7adc8646b301365a8c7b6fc71657
jher235 <tim668666@gmail.com>
2025-07-11 04:07:17 +0900

    [Feat] #26 심장 질환 섹션 응답 dto 작성

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/majordisease/HeartSection.java
@@ -0,0 +1,34 @@
+package org.sopt.bofit.domain.insurancereport.dto.response.majordisease;
+
+import org.sopt.bofit.domain.insurance.entity.benefit.Heart;
+import org.sopt.bofit.domain.insurance.entity.benefit.InsuranceBenefit;
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurancereport.entity.constant.CoverageStatus;
+
+public record HeartSection(
+	String coverageStatus,
+	MajorDiseaseSubSection acuteMyocardialInfarction,
+	MajorDiseaseSubSection ischemic,
+	MajorDiseaseSubSection extended,
+	MajorDiseaseSubSection arrhythmia
+) {
+	public static HeartSection of(CoverageStatus status, InsuranceProduct product, InsuranceBenefit average){
+		Heart productHeart = product.getMajorDisease().getHeart();
+		Heart averageHeart = average.getMajorDisease().getHeart();
+
+		return new HeartSection(status.getDescription(),
+			MajorDiseaseSubSection.of(
+				productHeart.getAcuteMyocardialInfarctionDiagnosis(), averageHeart.getAcuteMyocardialInfarctionDiagnosis(),
+				productHeart.getAcuteMyocardialInfarctionSurgery(), averageHeart.getAcuteMyocardialInfarctionSurgery()),
+			MajorDiseaseSubSection.of(
+				productHeart.getIschemicDiagnosis(), averageHeart.getIschemicDiagnosis(),
+				productHeart.getIschemicSurgery(), averageHeart.getIschemicSurgery()),
+			MajorDiseaseSubSection.of(
+				productHeart.getExtendedDiagnosis(), averageHeart.getExtendedDiagnosis(),
+				productHeart.getExtendedSurgery(), averageHeart.getExtendedSurgery()),
+			MajorDiseaseSubSection.of(
+				productHeart.getArrhythmiaDiagnosis(), averageHeart.getArrhythmiaDiagnosis(),
+				productHeart.getArrhythmiaSurgery(), averageHeart.getArrhythmiaSurgery())
+		);
+	}
+}
