c0088209377d307cc435191ebb4c42ffedbc6f3a
jher235 <tim668666@gmail.com>
2025-07-11 03:07:02 +0900

    [Feat] #26 리포트의 뇌혈관 질환 섹션 조회 Dto 작성

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/majordisease/CerebrovascularSection.java
@@ -0,0 +1,31 @@
+package org.sopt.bofit.domain.insurancereport.dto.response.majordisease;
+
+import org.sopt.bofit.domain.insurance.entity.benefit.Cerebrovascular;
+import org.sopt.bofit.domain.insurance.entity.benefit.InsuranceBenefit;
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurancereport.entity.constant.CoverageStatus;
+
+public record CerebrovascularSection(
+	String coverageStatus,
+	MajorDiseaseSubSection hemorrhage,
+	MajorDiseaseSubSection infarction,
+	MajorDiseaseSubSection other
+) {
+
+	public static CerebrovascularSection of(CoverageStatus status, InsuranceProduct product, InsuranceBenefit average){
+		Cerebrovascular productCerebrovascular = product.getMajorDisease().getCerebrovascular();
+		Cerebrovascular averageCerebrovascular = average.getMajorDisease().getCerebrovascular();
+
+		return new CerebrovascularSection(status.getDescription(),
+			MajorDiseaseSubSection.of(
+				productCerebrovascular.getHemorrhageDiagnosis(), averageCerebrovascular.getHemorrhageDiagnosis(),
+				productCerebrovascular.getHemorrhageSurgery(), averageCerebrovascular.getHemorrhageSurgery()),
+			MajorDiseaseSubSection.of(
+				productCerebrovascular.getInfarctionDiagnosis(), averageCerebrovascular.getInfarctionDiagnosis(),
+				productCerebrovascular.getInfarctionSurgery(), averageCerebrovascular.getInfarctionSurgery()),
+			MajorDiseaseSubSection.of(
+				productCerebrovascular.getOtherDiagnosis(), averageCerebrovascular.getOtherDiagnosis(),
+				productCerebrovascular.getOtherSurgery(), averageCerebrovascular.getOtherSurgery())
+			);
+	}
+}
