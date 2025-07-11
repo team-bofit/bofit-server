531d5ac47bba988518bab6880e8c5342eeadc786
jher235 <tim668666@gmail.com>
2025-07-12 03:28:45 +0900

    [Feat] #42 getter 추가

--- src/main/java/org/sopt/bofit/domain/insurancereport/constant/AdditionalInfo.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/constant/AdditionalInfo.java
@@ -1,7 +1,9 @@
 package org.sopt.bofit.domain.insurancereport.constant;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum AdditionalInfo {
 
