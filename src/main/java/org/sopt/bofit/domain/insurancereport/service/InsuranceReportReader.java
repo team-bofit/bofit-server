358d873b6ebc8305542e9b844725c6049df8ab43
jher235 <tim668666@gmail.com>
2025-07-12 00:09:11 +0900

    [Feat] #42 보험 상품 상세 조회 api 구현

--- src/main/java/org/sopt/bofit/domain/insurancereport/service/InsuranceReportReader.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/service/InsuranceReportReader.java
@@ -1,7 +1,13 @@
 package org.sopt.bofit.domain.insurancereport.service;
 
+import java.util.UUID;
+
 import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
+import org.sopt.bofit.domain.insurancereport.errorcode.InsuranceReportErrorCode;
 import org.sopt.bofit.domain.insurancereport.repository.InsuranceReportRepository;
+import org.sopt.bofit.global.exception.constant.InsuranceErrorCode;
+import org.sopt.bofit.global.exception.custom_exception.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +16,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class InsuranceReportReader {
 	private final InsuranceReportRepository insuranceReportRepository;
+
+	public InsuranceReport findById(UUID insuranceReportId){
+		return insuranceReportRepository.findById(insuranceReportId).orElseThrow(() ->
+			new NotFoundException(InsuranceReportErrorCode.NOT_FOUND_INSURANCE_REPORT));
+	}
+
+	public InsuranceReport findByIdWithRelatedEntity(UUID insuranceReportId){
+		return insuranceReportRepository.findById(insuranceReportId).orElseThrow(() ->
+			new NotFoundException(InsuranceReportErrorCode.NOT_FOUND_INSURANCE_REPORT));
+	}
 }
