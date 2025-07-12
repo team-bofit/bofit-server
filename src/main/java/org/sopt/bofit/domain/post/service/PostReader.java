2002654302533ac0a0b1891f56cbdcea90dbcf88
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-12 16:58:16 +0900

    [Feat] #43 전체 글 목록 조회 서비스 로직 구현

--- src/main/java/org/sopt/bofit/domain/post/service/PostReader.java
+++ src/main/java/org/sopt/bofit/domain/post/service/PostReader.java
@@ -1,9 +1,24 @@
 package org.sopt.bofit.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.post.dto.response.PostListResponse;
+import org.sopt.bofit.domain.post.repository.PostCustomRepositoryImpl;
+import org.sopt.bofit.domain.post.repository.PostRepository;
+import org.sopt.bofit.domain.user.dto.response.SliceResponse;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostReader {
+    private final PostRepository postRepository;
+
+    private final PostCustomRepositoryImpl postCustomRepositoryImpl;
+
+    public SliceResponse<PostListResponse> getAllPosts(Long cursorId, int size){
+
+        Slice<PostListResponse> postList = postCustomRepositoryImpl.findAllByCursorId(cursorId, size);
+
+        return SliceResponse.of(postList);
+    }
 }
