5ea7daccc5d63bfccb3320a84487437580a3a599
jher235 <tim668666@gmail.com>
2025-07-13 01:52:53 +0900

    [Feat] #45 레포트 조회 dto 작성 - 정적 부분도 서버에서 전달해줘야하는 소요가 발생하여 해당 dto 내에서 정적인 부분을 생성함. - 큰병, 수술비, 입원비, 장해, 사망에 대해 각각 SectionData를 만들어서 반환함

---
+++ src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/InsuranceReportResponse.java
@@ -0,0 +1,104 @@
+package org.sopt.bofit.domain.insurancereport.dto.response;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.sopt.bofit.domain.insurance.entity.benefit.Cancer;
+import org.sopt.bofit.domain.insurance.entity.product.BasicInformation;
+import org.sopt.bofit.domain.insurancereport.constant.AdditionalInfo;
+import org.sopt.bofit.domain.insurancereport.entity.Disease;
+import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
+import org.sopt.bofit.domain.insurancereport.entity.ReportRationale;
+
+import lombok.Builder;
+
+@Builder
+public record InsuranceReportResponse (
+	UUID reportId,
+	BasicInformation reportInformation,
+	ReportRationale reportRationale,
+	SectionData majorDisease,
+	SectionData surgery,
+	SectionData hospitalization,
+	SectionData disability,
+	SectionData death
+) {
+
+	public static InsuranceReportResponse from(InsuranceReport report){
+		return InsuranceReportResponse.builder()
+			.reportId(report.getId())
+			.reportInformation(report.getProduct().getBasicInformation())
+			.reportRationale(report.getReportRationale())
+			.majorDisease(SectionData.majorDisease(report))
+			.surgery(SectionData.surgery(report))
+			.hospitalization(SectionData.hospitalization(report))
+			.disability(SectionData.disability(report))
+			.death(SectionData.death(report))
+			.build();
+	}
+
+
+	private record SectionData(
+		String additionalInfo,
+		List<ShowCoverageStatus> statuses
+	){
+		private static SectionData majorDisease(InsuranceReport report){
+			return new SectionData(
+				AdditionalInfo.MAJOR_DISEASE.getInformation(),
+				List.of(
+					new ShowCoverageStatus(Disease.CANCER.getDisplayName(), report.getCancer().getDescription()),
+					new ShowCoverageStatus(Disease.CEREBROVASCULAR.getDisplayName(), report.getCerebrovascular().getDescription()),
+					new ShowCoverageStatus(Disease.HEART.getDisplayName(), report.getHeartDisease().getDescription())
+				)
+			);
+		}
+
+		private static SectionData surgery (InsuranceReport report){
+			return new SectionData(
+				AdditionalInfo.SURGERY.getInformation(),
+				List.of(
+					new ShowCoverageStatus(Disease.DISEASE_SURGERY.getDisplayName(), report.getDiseaseSurgery().getDescription()),
+					new ShowCoverageStatus(Disease.DISEASE_TYPE_SURGERY.getDisplayName(), report.getDiseaseTypeSurgery().getDescription()),
+					new ShowCoverageStatus(Disease.INJURY_SURGERY.getDisplayName(), report.getInjurySurgery().getDescription()),
+					new ShowCoverageStatus(Disease.INJURY_TYPE_SURGERY.getDisplayName(), report.getInjuryTypeSurgery().getDescription())
+				)
+			);
+		}
+
+		private static SectionData hospitalization (InsuranceReport report){
+			return new SectionData(
+				AdditionalInfo.HOSPITALIZATION.getInformation(),
+				List.of(
+					new ShowCoverageStatus(Disease.DISEASE_DAILY_HOSPITALIZATION.getDisplayName(), report.getDiseaseDailyHospitalization().getDescription()),
+					new ShowCoverageStatus(Disease.INJURY_DAILY_HOSPITALIZATION.getDisplayName(), report.getInjuryDailyHospitalization().getDescription())
+				)
+			);
+		}
+
+		private static SectionData disability (InsuranceReport report){
+			return new SectionData(
+				AdditionalInfo.DISABILITY.getInformation(),
+				List.of(
+					new ShowCoverageStatus(Disease.DISEASE_DISABILITY.getDisplayName(), report.getDiseaseDisability().getDescription()),
+					new ShowCoverageStatus(Disease.INJURY_DISABILITY.getDisplayName(), report.getInjuryDisability().getDescription())
+				)
+			);
+		}
+
+		private static SectionData death (InsuranceReport report){
+			return new SectionData(
+				AdditionalInfo.DEATH.getInformation(),
+				List.of(
+					new ShowCoverageStatus(Disease.DISEASE_DEATH.getDisplayName(), report.getDiseaseDeath().getDescription()),
+					new ShowCoverageStatus(Disease.INJURY_DEATH.getDisplayName(), report.getInjuryDeath().getDescription())
+				)
+			);
+		}
+
+	}
+	private record ShowCoverageStatus(
+		String target,
+		String status
+	){
+	}
+}
