14dc547611edbf1c4b538d0f3ff9d4d1884a94be
jher235 <tim668666@gmail.com>
2025-07-13 00:57:19 +0900

    [Feat] #45 입원 섹션 조회 dto 작성

--- src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/dailyHospitalization/DailyHospitalizationSection.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/dto/response/dailyHospitalization/DailyHospitalizationSection.java
@@ -1,19 +1,52 @@
 package org.sopt.bofit.domain.insurancereport.dto.response.dailyHospitalization;
 
+import static org.sopt.bofit.domain.insurancereport.entity.Disease.*;
+import static org.sopt.bofit.domain.insurancereport.errorcode.InsuranceReportErrorCode.*;
+
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurance.entity.statistic.InsuranceStatistic;
 import org.sopt.bofit.domain.insurancereport.dto.response.CompareCoverage;
+import org.sopt.bofit.domain.insurancereport.dto.response.majordisease.MajorDiseaseSection;
+import org.sopt.bofit.domain.insurancereport.entity.Disease;
+import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
 import org.sopt.bofit.domain.insurancereport.entity.constant.CoverageStatus;
+import org.sopt.bofit.global.exception.custom_exception.BadRequestException;
 
 public record DailyHospitalizationSection (
-	String coverageStatus,
+	String displayName,
 	CompareCoverage diseaseDailyHospitalization
 ){
-	public static DailyHospitalizationSection of(
-		CoverageStatus coverageStatus,
-		int productCoverage,
-		int averageCoverage
+
+	public static DailyHospitalizationSection getHospitalizationSection(String hyphenCase, InsuranceReport report){
+		InsuranceProduct product = report.getProduct();
+		InsuranceStatistic statistic = report.getStatistic();
+
+		if(DISEASE_DAILY_HOSPITALIZATION.getHyphenCase().equalsIgnoreCase(hyphenCase))
+			return createDisease(product, statistic);
+		if (INJURY_DAILY_HOSPITALIZATION.getHyphenCase().equalsIgnoreCase(hyphenCase))
+			return createInjury(product, statistic);
+
+		throw new BadRequestException(INVALID_REPORT_SECTION);
+	}
+
+	private static DailyHospitalizationSection createDisease(
+		InsuranceProduct product,
+		InsuranceStatistic average
 	){
 		return new DailyHospitalizationSection(
-			coverageStatus.getDescription(),
-			new CompareCoverage(productCoverage, averageCoverage));
+			Disease.DISEASE_DAILY_HOSPITALIZATION.getDisplayName(),
+			CompareCoverage.of(product.getDailyHospitalization().getDisease(),
+				average.getDailyHospitalization().getDisease()));
 	}
+
+	private static DailyHospitalizationSection createInjury(
+		InsuranceProduct product,
+		InsuranceStatistic average
+	){
+		return new DailyHospitalizationSection(
+			Disease.INJURY_DAILY_HOSPITALIZATION.getDisplayName(),
+			CompareCoverage.of(product.getDailyHospitalization().getInjury(),
+				average.getDailyHospitalization().getInjury()));
+	}
+
 }
