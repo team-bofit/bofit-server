bb56bf92d8ba4632cc3ca35595a8f9f9248e9c13
jher235 <tim668666@gmail.com>
2025-07-13 12:09:54 +0900

    [Feat] #54 comment 관련 상수 클래스 정의 - content 최대 크기 정의

---
+++ src/main/java/org/sopt/bofit/domain/comment/constant/CommentConstant.java
@@ -0,0 +1,9 @@
+package org.sopt.bofit.domain.comment.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentConstant {
+	public static final int COMMENT_CONTENT_MAX_SIZE = 30;
+}
