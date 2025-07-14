8ffc91a7552e9d1f3a318c22a2b22ce59e750a71
jher235 <tim668666@gmail.com>
2025-07-14 22:00:54 +0900

    [Feat] #56 comments 목록 조회 메서드 추가 - 커서 기반 페이지네이션

--- src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepository.java
+++ src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepository.java
@@ -1,8 +1,12 @@
 package org.sopt.bofit.domain.comment.repository;
 
+import java.util.Optional;
+
+import org.sopt.bofit.domain.comment.dto.response.CommentResponse;
 import org.sopt.bofit.domain.user.dto.response.MyCommentSummaryResponse;
 import org.springframework.data.domain.Slice;
 
 public interface CommentCustomRepository {
     Slice<MyCommentSummaryResponse> findCommentsByCursorId(Long userId, Long cursorId, int size);
+    Slice<CommentResponse> findActivesByPostIdWithCursor(Long postId, Optional<Long> cursor, int size);
 }
--- src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepositoryImpl.java
+++ src/main/java/org/sopt/bofit/domain/comment/repository/CommentCustomRepositoryImpl.java
@@ -3,8 +3,13 @@ package org.sopt.bofit.domain.comment.repository;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import org.sopt.bofit.domain.comment.dto.response.CommentResponse;
 import org.sopt.bofit.domain.comment.entity.CommentStatus;
+import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.post.entity.QPost;
 import org.sopt.bofit.domain.user.dto.response.MyCommentSummaryResponse;
+import org.sopt.bofit.domain.user.entity.QUser;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -12,16 +17,19 @@ import org.springframework.stereotype.Repository;
 import org.sopt.bofit.domain.comment.entity.QComment;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class CommentCustomRepositoryImpl implements CommentCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    private final QComment comment = QComment.comment;
+    private final QUser user = QUser.user;
 
     @Override
     public Slice<MyCommentSummaryResponse> findCommentsByCursorId(Long userId, Long cursorId, int size) {
-        QComment comment = QComment.comment;
+
 
         List<MyCommentSummaryResponse> content = queryFactory
                 .select(Projections.constructor(MyCommentSummaryResponse.class,
@@ -40,6 +48,40 @@ public class CommentCustomRepositoryImpl implements CommentCustomRepository {
                 .orderBy(comment.id.desc())
                 .limit(size + 1)
                 .fetch();
+
+        boolean hasNext = content.size() > size;
+        if (hasNext) content.remove(size);
+
+        return new SliceImpl<>(content, PageRequest.of(0, size), hasNext);
+    }
+
+
+    @Override
+    public Slice<CommentResponse> findActivesByPostIdWithCursor(
+        Long postId,
+        Optional<Long> cursor,
+        int size
+    ) {
+        List<CommentResponse> content = queryFactory
+            .select(Projections.constructor(CommentResponse.class,
+                comment.id,
+                user.nickname,
+                user.profileImage,
+                comment.content,
+                comment.createdAt,
+                comment.updatedAt
+            ))
+            .from(comment)
+            .leftJoin(user).on(user.eq(comment.user))
+            .where(
+                comment.post.id.eq(postId),
+                comment.status.eq(CommentStatus.ACTIVE),
+				cursor.map(comment.id::lt).orElse(null)
+            )
+            .groupBy(comment.id)
+            .orderBy(comment.id.desc())
+            .limit(size + 1)
+            .fetch();
 
         boolean hasNext = content.size() > size;
         if (hasNext) content.remove(size);
