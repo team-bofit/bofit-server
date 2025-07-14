bb6004de835dd7b8b9cb773ffa5a4e1d73cfc844
jher235 <tim668666@gmail.com>
2025-07-15 00:26:18 +0900

    [Feat] #62 보장 상황 목록을 조회하는 로직 구현

--- src/main/java/org/sopt/bofit/domain/user/service/UserService.java
+++ src/main/java/org/sopt/bofit/domain/user/service/UserService.java
@@ -1,8 +1,10 @@
 package org.sopt.bofit.domain.user.service;
 
+import org.sopt.bofit.domain.user.dto.response.CoveragePreferenceResponses;
 import org.sopt.bofit.domain.user.dto.response.MyCommentSummaryResponse;
 
 import org.sopt.bofit.domain.user.dto.response.MyPostSummaryResponse;
+import org.sopt.bofit.domain.user.entity.constant.CoveragePreference;
 import org.sopt.bofit.global.dto.response.SliceResponse;
 import org.sopt.bofit.domain.user.dto.response.DiagnosedDiseaseResponses;
 import org.sopt.bofit.domain.user.dto.response.JobResponses;
@@ -31,6 +33,10 @@ public class UserService {
 
 	public DiagnosedDiseaseResponses getDiagnosedDiseaseNames(){
 		return userReader.getDiagnosedDiseaseNames();
+	}
+
+	public CoveragePreferenceResponses  getCoveragePreference(){
+		return CoveragePreferenceResponses.from(CoveragePreference.values());
 	}
 
 	@Transactional
