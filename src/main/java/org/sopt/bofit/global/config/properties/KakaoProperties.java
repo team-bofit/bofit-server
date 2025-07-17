f5b5eefab8d352f427dfb96fee6dde200f0d0ad2
dlwjddus1112 <dlwjddus1112@naver.com>
2025-07-17 23:50:29 +0900

    [Chore] #97 KakaoProperties에서 redirectUri 삭제

--- src/main/java/org/sopt/bofit/global/config/properties/KakaoProperties.java
+++ src/main/java/org/sopt/bofit/global/config/properties/KakaoProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "kakao")
 public record KakaoProperties(
         String clientId,
-        String redirectUri,
         String tokenUri,
         String userInfoUri
 ) { }
