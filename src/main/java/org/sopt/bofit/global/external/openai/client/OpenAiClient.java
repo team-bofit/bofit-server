625d734b7b64206cd8339bf9adeba518ba690cb3
jher235 <tim668666@gmail.com>
2025-07-18 01:37:38 +0900

    [Feature] #99 LLMRetry 적용 - 현재는 400, 500 번대 에러에 대해서만 리트라이를 제대로 수행함. 또한 3번 실패 시 폴백을 통해 기본 설명이 날아감

--- src/main/java/org/sopt/bofit/global/external/openai/client/OpenAiClient.java
+++ src/main/java/org/sopt/bofit/global/external/openai/client/OpenAiClient.java
@@ -1,18 +1,33 @@
 package org.sopt.bofit.global.external.openai.client;
 
+import static org.sopt.bofit.domain.insurancereport.constant.InsuranceReportConstant.*;
+import static org.sopt.bofit.global.constant.ConfigConstant.*;
+import static org.sopt.bofit.global.exception.constant.GlobalErrorCode.*;
+
+import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
+import org.sopt.bofit.domain.insurancereport.entity.ReportRationale;
 import org.sopt.bofit.global.config.properties.OpenAiProperties;
+import org.sopt.bofit.global.exception.custom_exception.InternalException;
 import org.sopt.bofit.global.external.openai.dto.request.ChatRequestMessage;
 import org.sopt.bofit.global.external.openai.dto.request.OpenAiRequest;
 import org.sopt.bofit.global.external.openai.dto.response.OpenAiResponse;
 import org.sopt.bofit.global.oauth.constant.HttpHeaderConstants;
+import org.sopt.bofit.global.util.JsonUtil;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Component
 public class OpenAiClient {
 	public static final String REQUEST_URI = "/chat/completions";
@@ -31,19 +46,35 @@ public class OpenAiClient {
 
 	public String sendRequest(List<ChatRequestMessage> messages) {
 		OpenAiRequest request = new OpenAiRequest(
-				properties.model(),
-				messages,
-				properties.maxTokens(),
-				properties.temperature()
+			properties.model(),
+			messages,
+			properties.maxTokens(),
+			properties.temperature()
 		);
 
 		OpenAiResponse response = restClient.post()
-				.uri(REQUEST_URI)
-				.body(request)
-				.retrieve()
-				.body(OpenAiResponse.class);
+			.uri(REQUEST_URI)
+			.body(request)
+			.retrieve()
+			.body(OpenAiResponse.class);
 
 		return parseContent(Objects.requireNonNull(response));
+	}
+
+	@Retry(name = LLM_RETRY_NAME, fallbackMethod = "generateRationaleFallback")
+	public ReportRationale sendReportRelationalRequest(List<ChatRequestMessage> messages) {
+		try {
+			String responseString = sendRequest(messages);
+			return JsonUtil.parseClass(ReportRationale.class, responseString);
+		} catch (HttpClientErrorException | HttpServerErrorException e) {
+		// } catch (RuntimeException e) {
+			log.info("OpenAI Exception:{}", e.getMessage());
+			throw new InternalException(EXTERNAL_SERVER_ERROR);
+		}
+	}
+
+	public ReportRationale generateRationaleFallback(List<ChatRequestMessage> messages, Throwable t){
+		return new ReportRationale(DEFAULT_RATIONALE_REASONS, DEFAULT_RATIONAL_KEYWORD_CHIPS);
 	}
 
 	private String parseContent(OpenAiResponse response) {
