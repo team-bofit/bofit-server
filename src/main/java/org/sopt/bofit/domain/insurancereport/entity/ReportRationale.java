a814329b9ef2923f53a631af1bda1dba994d3854
jher235 <tim668666@gmail.com>
2025-07-18 01:23:00 +0900

    [Feature] #99 생성자 추가

--- src/main/java/org/sopt/bofit/domain/insurancereport/entity/ReportRationale.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/entity/ReportRationale.java
@@ -8,10 +8,15 @@ import org.sopt.bofit.global.converter.ListJsonConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReportRationale {
 	@Column(name = "reasons", columnDefinition = "JSON")
 	@Convert(converter = ListJsonConverter.class)
