680d87848cd4d85f436f8aa6bfe1d81abd72d887
jher235 <tim668666@gmail.com>
2025-08-01 23:50:50 +0900

    [chore] #114 @EnableCaching 추가

--- src/main/java/org/sopt/bofit/BofitApplication.java
+++ src/main/java/org/sopt/bofit/BofitApplication.java
@@ -6,8 +6,10 @@ import org.sopt.bofit.global.config.properties.OpenAiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties({JwtProperties.class, KakaoProperties.class, OpenAiProperties.class})
