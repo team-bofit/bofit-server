053de5253c098712c8822bb3b11b5380be0e96c2
jher235 <tim668666@gmail.com>
2025-08-02 00:46:46 +0900

    [Feat] #114 InsuranceStatistic 캐싱 - 구조 변경이 필요한데, 현재는 파라미터 없이getTotalAverage 메서드로 TotalAverage를 가져오므로 키를 지정해줄 수 없어서 SimpleKey.EMPTY 가 들어감. 캐싱은 정상적으로 이뤄지지만, 파라미터로 가져오도록 메서드를 변경하는 것이 구조적으로도 좋아보임.

--- src/main/java/org/sopt/bofit/domain/insurance/service/InsuranceStatisticReader.java
+++ src/main/java/org/sopt/bofit/domain/insurance/service/InsuranceStatisticReader.java
@@ -1,10 +1,13 @@
 package org.sopt.bofit.domain.insurance.service;
 
+import static org.sopt.bofit.global.constant.CacheConstant.*;
+
 import org.sopt.bofit.domain.insurance.entity.statistic.InsuranceStatistic;
 import org.sopt.bofit.domain.insurance.entity.statistic.StatisticRange;
 import org.sopt.bofit.domain.insurance.repository.InsuranceStatisticRepository;
 import org.sopt.bofit.global.exception.constant.InsuranceErrorCode;
 import org.sopt.bofit.global.exception.customexception.InternalException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class InsuranceStatisticReader {
 	private final InsuranceStatisticRepository insuranceStatisticRepository;
 
+	@Cacheable(value = INSURANCE_STATISTIC_CACHE_NAME)
 	public InsuranceStatistic getTotalAverage (){
 		return insuranceStatisticRepository.findByStatisticRange(StatisticRange.TOTAL_AVERAGE).orElseThrow(() ->
 			new InternalException(InsuranceErrorCode.NOT_FOUND_INSURANCE_TOTAL_AVERAGE));
