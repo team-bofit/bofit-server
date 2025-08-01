ff172d423b64389f4e22179705e7b8e3c4bded49
jher235 <tim668666@gmail.com>
2025-08-02 00:44:48 +0900

    [Feat] #114 ScoringRule Read 시 캐싱

--- src/main/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/ScoringRuleProvider.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/ScoringRuleProvider.java
@@ -1,5 +1,7 @@
 package org.sopt.bofit.domain.insurancereport.service.scoringrule;
 
+import static org.sopt.bofit.global.constant.CacheConstant.*;
+
 import java.util.List;
 
 import org.sopt.bofit.domain.insurancereport.entity.scoringrule.diseasehistory.DiseaseHistoryScoringRule;
@@ -13,6 +15,7 @@ import org.sopt.bofit.domain.insurancereport.repository.scoringrule.SelectedScor
 import org.sopt.bofit.domain.insurancereport.repository.scoringrule.UserInfoScoringRuleRepository;
 import org.sopt.bofit.domain.user.entity.constant.CoveragePreference;
 import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -25,18 +28,22 @@ public class ScoringRuleProvider {
 	private final FamilyHistoryScoringRuleRepository familyHistoryScoringRuleRepository;
 	private final SelectedScoringRuleRepository selectedScoringRuleRepository;
 
+	@Cacheable(cacheNames = USER_INFO_SCORING_RULE_CACHE_NAME, key = "#userInfoRuleType")
 	public List<UserInfoScoringRule> findAllUserInfoRuleType(UserInfoRuleType userInfoRuleType){
 		return userInfoScoringRuleRepository.findAllByUserInfoRuleType(userInfoRuleType);
 	}
 
+	@Cacheable(cacheNames = DISEASE_HISTORY_SCORING_RULE_CACHE_NAME, key = "#diagnosedDisease")
 	public List<DiseaseHistoryScoringRule> findAllDiseaseHistory(DiagnosedDisease diagnosedDisease){
 		return diseaseHistoryScoringRuleRepository.findAllByDiagnosedDisease(diagnosedDisease);
 	}
 
+	@Cacheable(cacheNames = FAMILY_HISTORY_SCORING_RULE_CACHE_NAME, key = "#diagnosedDisease")
 	public List<FamilyHistoryScoringRule> findAllFamilyHistory(DiagnosedDisease diagnosedDisease){
 		return familyHistoryScoringRuleRepository.findAllByDiagnosedDisease(diagnosedDisease);
 	}
 
+	@Cacheable(cacheNames = SELECTED_SCORING_RULE_CACHE_NAME, key = "#coveragePreference")
 	public List<SelectedScoringRule> findAllCoveragePreference(CoveragePreference coveragePreference){
 		return selectedScoringRuleRepository.findAllByCoveragePreference(coveragePreference);
 	}
