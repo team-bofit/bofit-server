412a889bae96e45c106c7385bfc7ccc575cecad2
jher235 <tim668666@gmail.com>
2025-08-02 00:22:27 +0900

    [Feat] #114 캐시 상수 클래스 작성

---
+++ src/main/java/org/sopt/bofit/global/constant/CacheConstant.java
@@ -0,0 +1,18 @@
+ package org.sopt.bofit.global.constant;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CacheConstant {
+
+	public final static String DEFAULT_CACHE_NAME = "default";
+	public final static String USER_INFO_SCORING_RULE_CACHE_NAME = "userInfoScoringRule";
+	public final static String DISEASE_HISTORY_SCORING_RULE_CACHE_NAME = "diseaseHistoryScoringRule";
+	public final static String FAMILY_HISTORY_SCORING_RULE_CACHE_NAME = "familyHistoryScoringRule";
+	public final static String SELECTED_SCORING_RULE_CACHE_NAME = "selectedScoringRule";
+	public final static String INSURANCE_STATISTIC_CACHE_NAME = "insuranceStatistic";
+	public final static String INSURANCE_REPORT_CACHE_NAME = "insuranceReport";
+
+}
