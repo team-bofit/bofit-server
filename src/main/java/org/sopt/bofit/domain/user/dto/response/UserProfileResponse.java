6e1a8a9ecc37cb523e67c5d449a909af7b5b192b
jher235 <tim668666@gmail.com>
2025-08-07 20:42:03 +0900

    [Refactor] #119 정적 팩토리 메서드를 from 으로 수정 - 기존 of 방식을 사용한다면, 수정이 발생 시 dto 내에서만 처리하는 것이 아니라 서비스 단도 함께 수정해야하는 소요가 발생하므로 방식을 수정함

--- src/main/java/org/sopt/bofit/domain/user/dto/response/UserProfileResponse.java
+++ src/main/java/org/sopt/bofit/domain/user/dto/response/UserProfileResponse.java
@@ -1,6 +1,9 @@
 package org.sopt.bofit.domain.user.dto.response;
 
+import org.sopt.bofit.domain.user.entity.User;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 public record UserProfileResponse(
         @Schema(description = "유저 PK", example = "1")
@@ -21,6 +24,26 @@ public record UserProfileResponse(
 
     public static UserProfileResponse of(Long userId, String username, String nickname, String profileImageUrl, boolean isRecommendInsurance) {
         return new UserProfileResponse(userId, username, nickname, profileImageUrl, isRecommendInsurance);
+    }
+
+    @Builder
+    public UserProfileResponse(Long userId, String username, String nickname, String profileImageUrl,
+        boolean isRecommendInsurance) {
+        this.userId = userId;
+        this.username = username;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.isRecommendInsurance = isRecommendInsurance;
+    }
+
+    public static UserProfileResponse from(User user) {
+        return UserProfileResponse.builder()
+            .userId(user.getId())
+            .username(user.getName())
+            .nickname(user.getNickname())
+            .profileImageUrl(user.getProfileImage())
+            .isRecommendInsurance(user.isRecommendInsurance())
+            .build();
     }
 
 }
