4d300f25272e1f73421d6adaf6e0e2d165e9a95f
jher235 <tim668666@gmail.com>
2025-08-08 13:05:57 +0900

    [Feature] #119 coverageStatusFromReport 필드 추가 및 대분류 diseaseSectionMap 생성 - diseaseSectionMap 으로 추후 항목이 추가되어도 관리가 편하도록 수정함

--- src/main/java/org/sopt/bofit/domain/insurancereport/entity/Disease.java
+++ src/main/java/org/sopt/bofit/domain/insurancereport/entity/Disease.java
@@ -1,5 +1,11 @@
 package org.sopt.bofit.domain.insurancereport.entity;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.sopt.bofit.domain.insurancereport.entity.constant.CoverageStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,43 +13,90 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Disease {
 
-	MAJOR_DISEASE("큰 병", "major-disease"),
+	MAJOR_DISEASE("큰 병", "major-disease", InsuranceReport::getMajorDisease),
 
-	CANCER("암", "cancer"),
-	GENERAL_CANCER("일반암", "general-cancer"),
-	ATYPICAL_CANCER("소액암", "atypical-cancer"),
+	CANCER("암", "cancer", InsuranceReport::getCancer),
+	GENERAL_CANCER("일반암", "general-cancer", report -> null),
+	ATYPICAL_CANCER("소액암", "atypical-cancer", report -> null),
 
-	CEREBROVASCULAR("뇌혈관질환", "cerebrovascular"),
-	CEREBRAL_HEMORRHAGE("뇌출혈", "cerebral-hemorrhage"),
-	CEREBRAL_INFARCTION("뇌경색", "cerebral-infarction"),
-	OTHER_CEREBROVASCULAR("기타 뇌혈관질환", "other-cerebrovascular"),
+	CEREBROVASCULAR("뇌혈관질환", "cerebrovascular", InsuranceReport::getCerebrovascular),
+	CEREBRAL_HEMORRHAGE("뇌출혈", "cerebral-hemorrhage", report -> null),
+	CEREBRAL_INFARCTION("뇌경색", "cerebral-infarction", report -> null),
+	OTHER_CEREBROVASCULAR("기타 뇌혈관질환", "other-cerebrovascular", report -> null),
 
-	HEART("심장질환", "heart-disease"),
-	ACUTE_MYOCARDIAL_INFARCTION("급성 심근경색", "acute-myocardial-infarction"),
-	ARRHYTHMIA_HEART_FAILURE("부정맥, 심부전", "arrhythmia-heart-failure"),
-	EXTENDED_HEART("확대 심장질환", "extended-heart-disease"),
-	ISCHEMIC_HEART("허혈성 심장질환", "ischemic-heart-disease"),
+	HEART("심장질환", "heart-disease", InsuranceReport::getHeartDisease),
+	ACUTE_MYOCARDIAL_INFARCTION("급성 심근경색", "acute-myocardial-infarction", report -> null),
+	ARRHYTHMIA_HEART_FAILURE("부정맥, 심부전", "arrhythmia-heart-failure", report -> null),
+	EXTENDED_HEART("확대 심장질환", "extended-heart-disease", report -> null),
+	ISCHEMIC_HEART("허혈성 심장질환", "ischemic-heart-disease", report -> null),
 
-	SURGERY("수술", "surgery"),
-	DISEASE_SURGERY("질병수술비", "disease-surgery"),
-	DISEASE_TYPE_SURGERY("질병 종 수술비", "disease-type-surgery"),
-	INJURY_SURGERY("상해수술비", "injury-surgery"),
-	INJURY_TYPE_SURGERY("상해 종 수술비", "injury-type-surgery"),
+	SURGERY("수술", "surgery", InsuranceReport::getSurgery),
+	DISEASE_SURGERY("질병수술비", "disease-surgery", InsuranceReport::getDiseaseSurgery),
+	DISEASE_TYPE_SURGERY("질병 종 수술비", "disease-type-surgery", InsuranceReport::getDiseaseTypeSurgery),
+	INJURY_SURGERY("상해수술비", "injury-surgery", InsuranceReport::getInjurySurgery),
+	INJURY_TYPE_SURGERY("상해 종 수술비", "injury-type-surgery", InsuranceReport::getInjuryTypeSurgery),
 
-	HOSPITALIZATION("입원", "hospitalization"),
-	DISEASE_DAILY_HOSPITALIZATION("질병입원일당(1일이상)", "disease-daily-hospitalization"),
-	INJURY_DAILY_HOSPITALIZATION("상해입원일당(1일이상)", "injury-daily-hospitalization"),
+	HOSPITALIZATION("입원", "hospitalization", InsuranceReport::getDailyHospitalization),
+	DISEASE_DAILY_HOSPITALIZATION("질병입원일당(1일이상)", "disease-daily-hospitalization", InsuranceReport::getDiseaseDailyHospitalization),
+	INJURY_DAILY_HOSPITALIZATION("상해입원일당(1일이상)", "injury-daily-hospitalization", InsuranceReport::getInjuryDailyHospitalization),
 
-	DISABILITY("장해", "disability"),
-	DISEASE_DISABILITY("질병후유장해", "disease-disability"),
-	INJURY_DISABILITY("상해후유장해", "injury-disability"),
+	DISABILITY("장해", "disability", InsuranceReport::getDisability),
+	DISEASE_DISABILITY("질병후유장해", "disease-disability", InsuranceReport::getDiseaseDisability),
+	INJURY_DISABILITY("상해후유장해", "injury-disability", InsuranceReport::getInjuryDisability),
 
-	DEATH("사망", "death"),
-	DISEASE_DEATH("질병사망", "disease-death"),
-	INJURY_DEATH("상해사망", "injury-death")
+	DEATH("사망", "death", InsuranceReport::getDeath),
+	DISEASE_DEATH("질병사망", "disease-death", InsuranceReport::getDiseaseDeath),
+	INJURY_DEATH("상해사망", "injury-death", InsuranceReport::getInjuryDeath)
 	;
 
 	private final String displayName;
 	private final String hyphenCase;
+	private final Function<InsuranceReport,CoverageStatus> coverageStatusFromReport;
+
+	public static final Map<Disease, List<Disease>> diseaseSectionMap = Map.of(
+		MAJOR_DISEASE, getMajorDiseaseSections(),
+		SURGERY, getSurgerySections(),
+		HOSPITALIZATION, getHospitalizationSections(),
+		DISABILITY, getDisabilitySections(),
+		DEATH, getDeathSections()
+	);
+
+	public static List<Disease> getMajorDiseaseSections(){
+		return List.of(
+			CANCER,
+			CEREBROVASCULAR,
+			HEART
+		);
+	}
+
+	public static List<Disease> getSurgerySections(){
+		return List.of(
+			DISEASE_SURGERY,
+			DISEASE_TYPE_SURGERY,
+			INJURY_SURGERY,
+			INJURY_TYPE_SURGERY
+		);
+	}
+
+	public static List<Disease> getHospitalizationSections(){
+		return List.of(
+			DISEASE_DAILY_HOSPITALIZATION,
+			INJURY_DAILY_HOSPITALIZATION
+		);
+	}
+
+	public static List<Disease> getDisabilitySections(){
+		return List.of(
+			DISEASE_DISABILITY,
+			INJURY_DISABILITY
+		);
+	}
+
+	public static List<Disease> getDeathSections(){
+		return List.of(
+			DISEASE_DEATH,
+			INJURY_DEATH
+		);
+	}
 
 }
