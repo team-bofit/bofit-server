5cafd607a72657804bdc0abbfc74d8231e4d29ca
jher235 <tim668666@gmail.com>
2025-08-15 22:51:20 +0900

    [Test] #121 ScoringRuleCalculator 테스트 작성

---
+++ src/test/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/FamilyHistoryRuleCalculatorTest.java
@@ -0,0 +1,105 @@
+package org.sopt.bofit.domain.insurancereport.service.scoringrule;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionCoverage.*;
+import static org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionOperator.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sopt.bofit.domain.insurance.InsuranceProductTestBuilder;
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionOperator;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.familyhistory.FamilyHistoryScoringRule;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.FamilyHistoryScoringRuleRepository;
+import org.sopt.bofit.domain.insurancereport.util.ScoringRuleUtil;
+import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FamilyHistoryRuleCalculatorTest {
+
+	@Autowired
+	private FamilyHistoryRuleCalculator familyHistoryRuleCalculator;
+
+	@Autowired
+	private FamilyHistoryScoringRuleRepository familyHistoryScoringRuleRepository;
+
+	@AfterEach
+	void clearInAfterTest(){
+		familyHistoryScoringRuleRepository.deleteAllInBatch();
+	}
+
+	@DisplayName("가족력과 관련된 ScoringRule 점수를 정상적으로 계산한다.")
+	@Test
+	void calculateTest(){
+		// given
+		InsuranceProduct product = new InsuranceProductTestBuilder()
+			.withGeneralCancerDiagnosis(5000)
+			.withGeneralCancerSurgery(1200)
+			.withDailyHospitalizationDisease(50)
+			.withIschemicDiagnosis(1000)
+			.build();
+
+		FamilyHistoryScoringRule appliedRule1 = FamilyHistoryScoringRule.create(
+			DiagnosedDisease.CANCER,
+			GENERAL_CANCER_DIAGNOSIS,
+			GE,
+			5000,
+			5d
+		);
+
+		FamilyHistoryScoringRule appliedRule2 = FamilyHistoryScoringRule.create(
+			DiagnosedDisease.CANCER,
+			GENERAL_CANCER_SURGERY,
+			GE,
+			1000,
+			5d
+		);
+
+		FamilyHistoryScoringRule appliedRule3 = FamilyHistoryScoringRule.create(
+			DiagnosedDisease.CHRONIC,
+			DAILY_HOSPITALIZATION_DISEASE,
+			EXIST,
+			0,
+			3d
+		);
+
+		FamilyHistoryScoringRule noneAppliedRule1 = FamilyHistoryScoringRule.create(
+			DiagnosedDisease.HEART,
+			ISCHEMIC_HEART_DISEASE_DIAGNOSIS,
+			ConditionOperator.GE,
+			3000,
+			10d
+		);
+
+		familyHistoryScoringRuleRepository.saveAll(List.of(
+			appliedRule1,
+			appliedRule2,
+			appliedRule3,
+			noneAppliedRule1
+		));
+
+		List<DiagnosedDisease> diagnosedDiseases = List.of(
+			DiagnosedDisease.CANCER,
+			DiagnosedDisease.CHRONIC,
+			DiagnosedDisease.HEART
+		);
+
+		// when
+		double result = familyHistoryRuleCalculator.calculate(diagnosedDiseases, product);
+
+		// then
+		assertThat(result).isEqualTo(
+			ScoringRuleUtil.getPoint(appliedRule1, product)  +
+				ScoringRuleUtil.getPoint(appliedRule2, product) +
+				ScoringRuleUtil.getPoint(appliedRule3, product) +
+				ScoringRuleUtil.getPoint(noneAppliedRule1, product)
+		);
+	}
+}
---
+++ src/test/java/org/sopt/bofit/domain/insurancereport/service/scoringrule/SelectedRuleCalculatorTest.java
@@ -0,0 +1,113 @@
+package org.sopt.bofit.domain.insurancereport.service.scoringrule;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionCoverage.*;
+import static org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionOperator.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sopt.bofit.domain.insurance.InsuranceProductTestBuilder;
+import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.ConditionOperator;
+import org.sopt.bofit.domain.insurancereport.entity.scoringrule.select.SelectedScoringRule;
+import org.sopt.bofit.domain.insurancereport.repository.scoringrule.SelectedScoringRuleRepository;
+import org.sopt.bofit.domain.insurancereport.util.ScoringRuleUtil;
+import org.sopt.bofit.domain.user.entity.constant.CoveragePreference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SelectedRuleCalculatorTest {
+
+	private final static double FIRST_WEIGHT = 1.2;
+	private final static double SECOND_WEIGHT = 1.0;
+	private final static double THIRD_WEIGHT = 0.8;
+
+	@Autowired
+	private SelectedRuleCalculator selectedRuleCalculator;
+
+	@Autowired
+	private SelectedScoringRuleRepository selectedScoringRuleRepository;
+
+	@AfterEach
+	void clearInAfterTest(){
+		selectedScoringRuleRepository.deleteAllInBatch();
+	}
+
+	@DisplayName("선택된 우선 보장 항목에 해당하는 ScoringRule 점수를 정상적으로 계산함")
+	@Test
+	void calculateTest(){
+		// given
+		int productPremium = 10000;
+		InsuranceProduct product = new InsuranceProductTestBuilder()
+			.withPremium(productPremium)
+			.withGeneralCancerDiagnosis(5000)
+			.withGeneralCancerSurgery(1000)
+			.withAtypicalCancerDiagnosis(1000)
+			.build();
+
+		SelectedScoringRule appliedRule1 = SelectedScoringRule.create(
+			CoveragePreference.ESSENTIAL_ONLY,
+			PREMIUM,
+			MINUS,
+			0,
+			-0.5d
+		);
+
+		SelectedScoringRule appliedRule2 = SelectedScoringRule.create(
+			CoveragePreference.MAJOR_DISEASE,
+			GENERAL_CANCER_DIAGNOSIS,
+			GE,
+			5000,
+			10d
+		);
+
+		SelectedScoringRule appliedRule3 = SelectedScoringRule.create(
+			CoveragePreference.MAJOR_DISEASE,
+			GENERAL_CANCER_SURGERY,
+			GE,
+			1000,
+			10d
+		);
+
+		SelectedScoringRule noneAppliedRule1 = SelectedScoringRule.create(
+			CoveragePreference.MAJOR_DISEASE,
+			ATYPICAL_CANCER_DIAGNOSIS,
+			ConditionOperator.GE,
+			5000,
+			10d
+		);
+
+		selectedScoringRuleRepository.saveAll(List.of(
+			appliedRule1,
+			appliedRule2,
+			appliedRule3,
+			noneAppliedRule1
+		));
+
+		Map<CoveragePreference, Integer> selectedCoverages = Map.of(
+			CoveragePreference.ESSENTIAL_ONLY, 1,
+			CoveragePreference.SURGERY_COVERAGE, 2,
+			CoveragePreference.MAJOR_DISEASE, 3
+		);
+
+		// when
+		double result = selectedRuleCalculator.calculate(selectedCoverages, product);
+
+		// then
+
+		assertThat(result).isEqualTo(
+			FIRST_WEIGHT * ScoringRuleUtil.getPoint(appliedRule1, product)  +
+				THIRD_WEIGHT * ScoringRuleUtil.getPoint(appliedRule2, product) +
+				THIRD_WEIGHT * ScoringRuleUtil.getPoint(appliedRule3, product) +
+				THIRD_WEIGHT * ScoringRuleUtil.getPoint(noneAppliedRule1, product)
+		);
+	}
+
+}
