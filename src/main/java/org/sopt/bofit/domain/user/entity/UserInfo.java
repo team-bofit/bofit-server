66f159aa0f693dfb25fffcb3821c8484176c7abe
jher235 <tim668666@gmail.com>
2025-08-18 01:44:28 +0900

    [Refactor] #121 JsonType.class 설정

--- src/main/java/org/sopt/bofit/domain/user/entity/UserInfo.java
+++ src/main/java/org/sopt/bofit/domain/user/entity/UserInfo.java
@@ -1,8 +1,11 @@
 package org.sopt.bofit.domain.user.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.hibernate.annotations.Type;
 import org.sopt.bofit.domain.insurancereport.entity.InsuranceReport;
 import org.sopt.bofit.domain.user.entity.constant.CoveragePreference;
 import org.sopt.bofit.domain.user.entity.constant.DiagnosedDisease;
@@ -10,10 +13,23 @@ import org.sopt.bofit.global.converter.CoveragePreferenceMapConverter;
 import org.sopt.bofit.global.converter.DiseaseEnumListJsonConverter;
 import org.sopt.bofit.global.entity.BaseEntity;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -38,14 +54,17 @@ public class UserInfo extends BaseEntity {
 
     private int maxPrice;
 
+    @Type(JsonType.class)
     @Convert(converter = DiseaseEnumListJsonConverter.class)
     @Column(name = "disease_history", columnDefinition = "JSON")
     private List<DiagnosedDisease> diseaseHistory = new ArrayList<>();
 
+    @Type(JsonType.class)
     @Convert(converter = DiseaseEnumListJsonConverter.class)
     @Column(name = "family_history", columnDefinition = "JSON")
     private List<DiagnosedDisease> familyHistory = new ArrayList<>();
 
+    @Type(JsonType.class)
     @Convert(converter = CoveragePreferenceMapConverter.class)
     @Column(name = "coverage_preference", columnDefinition = "JSON")
     private Map<CoveragePreference, Integer> coveragePreferences = new LinkedHashMap<>();
