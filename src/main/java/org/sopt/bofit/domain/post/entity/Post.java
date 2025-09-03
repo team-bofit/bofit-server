31cd05746c17edbb3fd391ef54772e06986bf59a
jher235 <tim668666@gmail.com>
2025-09-03 16:21:46 +0900

    [Feature] #125 LikeCount 칼럼 추가

--- src/main/java/org/sopt/bofit/domain/post/entity/Post.java
+++ src/main/java/org/sopt/bofit/domain/post/entity/Post.java
@@ -1,9 +1,22 @@
 package org.sopt.bofit.domain.post.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.util.Objects;
-
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.sopt.bofit.domain.post.entity.constant.PostStatus;
 import org.sopt.bofit.domain.user.entity.User;
 import org.sopt.bofit.global.entity.BaseEntity;
@@ -36,11 +49,15 @@ public class Post extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    private Long likeCount;
+
     public static Post create(User user, String title, String content) {
         return Post.builder()
                 .user(user)
                 .title(title)
                 .content(content)
+                .likeCount(0L)
                 .status(PostStatus.ACTIVE)
                 .build();
     }
