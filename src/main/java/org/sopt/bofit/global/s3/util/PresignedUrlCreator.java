e19eee03c8e85130ead6dcb1a0b6eb22eedefe81
dlwjddus1112 <dlwjddus1112@naver.com>
2025-09-04 15:58:42 +0900

    [Feat] #126 presigned URL 발급 클래스 구현

---
+++ src/main/java/org/sopt/bofit/global/s3/util/PresignedUrlCreator.java
@@ -0,0 +1,36 @@
+package org.sopt.bofit.global.s3.util;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.global.config.S3Config;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class PresignedUrlCreator {
+
+    private final S3Presigner presigner;
+
+    private final S3Config s3Config;
+
+    private final long ttlMinutes = 10;
+
+    public String createPutUrl(String key, String contentType) {
+        PutObjectRequest put = PutObjectRequest.builder()
+                .bucket(s3Config.getProperties().s3().bucket())
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        PresignedPutObjectRequest req = presigner.presignPutObject(b -> b
+                .putObjectRequest(put)
+                .signatureDuration(Duration.ofMinutes(ttlMinutes)));
+
+        return req.url().toString();
+    }
+    
+}
