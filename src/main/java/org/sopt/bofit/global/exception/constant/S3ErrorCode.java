05d222ae4bb3e1cc9d454f24d8aeacf3c7e155d0
dlwjddus1112 <dlwjddus1112@naver.com>
2025-09-04 19:32:30 +0900

    [Chore] #126 NOT_IMAGE ErrorCode 삭제

--- src/main/java/org/sopt/bofit/global/exception/constant/S3ErrorCode.java
+++ src/main/java/org/sopt/bofit/global/exception/constant/S3ErrorCode.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum S3ErrorCode implements ErrorCode {
-    NOT_IMAGE(HttpStatus.BAD_REQUEST.value(), "이미지 업로드만 지원합니다. 이미지를 업로드 해주세요."),
     UNSUPPORTED_MEDIA_TYPE(HttpStatus.BAD_REQUEST.value(), "지원하지 않는 확장자 형식입니다."),
     UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST.value(), "이미지는 .jpg, .jpeg, .png, .webp 형식만 지원합니다.")
     ;
