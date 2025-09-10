8ba8c3bdeac62a102ebea0db612ff92cd76a5ca3
jher235 <tim668666@gmail.com>
2025-09-10 22:19:04 +0900

    [Refactor] #136 대댓글 생성 Service Dto 작성 CommentReplyRequest 에서 toCommand 메서드로 변환 작업을 수행

--- src/main/java/org/sopt/bofit/domain/commentreply/dto/request/CommentReplyRequest.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/dto/request/CommentReplyRequest.java
@@ -5,7 +5,9 @@ import static org.sopt.bofit.global.file.constant.ImageConstant.MAX_IMAGE_URL_LE
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import java.util.Optional;
 import org.hibernate.validator.constraints.Length;
+import org.sopt.bofit.domain.commentreply.service.dto.request.CommentReplyCreateCommand;
 
 public record CommentReplyRequest (
     @Schema(description = "본문", example = "대댓글 작성하기 ~")
@@ -16,5 +18,8 @@ public record CommentReplyRequest (
     @Length(max = MAX_IMAGE_URL_LENGTH, message = "이미지 url 의 최대 길이 ({max}) 를 초과했습니다.")
     String imageUrl
 ){
+    public CommentReplyCreateCommand toCommand(){
+        return new CommentReplyCreateCommand(this.content, Optional.of(this.imageUrl));
+    }
 
 }
