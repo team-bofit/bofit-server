90677ca39396baed04d0b5686b514de082fcdee9
jher235 <tim668666@gmail.com>
2025-09-10 18:05:15 +0900

    [Feat] #136 대댓글 관련 상수 클래스 생성

---
+++ src/main/java/org/sopt/bofit/domain/commentreply/constant/CommentReplyConstant.java
@@ -0,0 +1,11 @@
+package org.sopt.bofit.domain.commentreply.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentReplyConstant {
+
+    public static final int MAX_CONTENT_LENGTH = 300;
+
+}
