7ce918c1477528c0d3d5d833d02302fa57628c6b
jher235 <tim668666@gmail.com>
2025-09-10 19:10:02 +0900

    [Feat] #136 대댓글 생성 presentation layer 작성

--- src/main/java/org/sopt/bofit/domain/post/controller/PostController.java
+++ src/main/java/org/sopt/bofit/domain/post/controller/PostController.java
@@ -1,13 +1,30 @@
 package org.sopt.bofit.domain.post.controller;
 
+import static org.sopt.bofit.domain.comment.constant.CommentConstant.COMMENTS_DEFAULT_SIZE;
+import static org.sopt.bofit.domain.post.constant.PostConstant.POSTS_DEFAULT_SIZE;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.CREATE_COMMENT;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.CREATE_COMMENT_REPLY;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.CREATE_POST;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.CREATE_POST_LIKE;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.DELETE_COMMENT;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.DELETE_POST;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.DELETE_POST_LIKE;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.POST_DETAIL;
+import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.UPDATE_POST;
+import static org.sopt.bofit.global.constant.SwaggerConstant.TAG_DESCRIPTION_COMMUNITY;
+import static org.sopt.bofit.global.constant.SwaggerConstant.TAG_NAME_COMMUNITY;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.sopt.bofit.domain.comment.dto.request.CommentCreateRequest;
 import org.sopt.bofit.domain.comment.dto.response.CommentResponse;
 import org.sopt.bofit.domain.comment.service.CommentService;
+import org.sopt.bofit.domain.commentreply.dto.request.CommentReplyRequest;
+import org.sopt.bofit.domain.commentreply.service.CommentReplyService;
 import org.sopt.bofit.domain.post.dto.request.PostCreateRequest;
 import org.sopt.bofit.domain.post.dto.request.PostUpdateRequest;
 import org.sopt.bofit.domain.post.dto.response.PostCreateResponse;
@@ -19,15 +36,15 @@ import org.sopt.bofit.global.annotation.CustomExceptionDescription;
 import org.sopt.bofit.global.annotation.LoginUserId;
 import org.sopt.bofit.global.dto.response.BaseResponse;
 import org.sopt.bofit.global.dto.response.SliceResponse;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
-import static org.sopt.bofit.domain.comment.constant.CommentConstant.COMMENTS_DEFAULT_SIZE;
-import static org.sopt.bofit.domain.post.constant.PostConstant.POSTS_DEFAULT_SIZE;
-import static org.sopt.bofit.global.config.swagger.SwaggerResponseDescription.*;
-import static org.sopt.bofit.global.constant.SwaggerConstant.TAG_DESCRIPTION_COMMUNITY;
-import static org.sopt.bofit.global.constant.SwaggerConstant.TAG_NAME_COMMUNITY;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,6 +54,7 @@ public class PostController {
     private final PostService postService;
     private final CommentService commentService;
     private final PostLikeService postLikeService;
+    private final CommentReplyService commentReplyService;
 
     @Tag(name = TAG_NAME_COMMUNITY, description = TAG_DESCRIPTION_COMMUNITY)
     @Operation(summary = "게시물 작성", description = "커뮤니티에 글을 작성합니다.")
@@ -155,6 +173,23 @@ public class PostController {
     ){
         postLikeService.deletePostLike(userId, postId);
         return BaseResponse.create("좋아요 삭제 성공");
+    }
+
+    @Tag(name = TAG_NAME_COMMUNITY, description = TAG_DESCRIPTION_COMMUNITY)
+    @Operation(summary = "대댓글 작성", description = "유저가 커뮤니티 댓글에 대댓글을 작성합니다.")
+    @CustomExceptionDescription(CREATE_COMMENT_REPLY)
+    @PostMapping("/{post-id}/comments/{comment-id}/reply")
+    public BaseResponse<Void> createCommentReply(
+        @PathVariable(name = "post-id") Long postId,
+        @PathVariable(name = "comment-id") Long commentId,
+        @Parameter(hidden = true) @LoginUserId Long userId,
+        @RequestBody CommentReplyRequest commentReplyRequest
+    ){
+        commentReplyService.create(
+            userId, postId, commentId,
+            commentReplyRequest.content(),
+            Optional.of(commentReplyRequest.imageUrl()));
+        return BaseResponse.create("대댓글 작성 성공");
     }
 
 }
