d265868b31e55dedf98eaae8c09537dd32d76cba
jher235 <tim668666@gmail.com>
2025-09-10 22:11:34 +0900

    [Feat] #136 댓글 이미지 엔티티 작성

---
+++ src/main/java/org/sopt/bofit/domain/comment/entity/CommentImage.java
@@ -0,0 +1,43 @@
+package org.sopt.bofit.domain.comment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.sopt.bofit.global.entity.BaseEntity;
+import org.sopt.bofit.global.file.constant.ImageConstant;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Comment comment;
+
+    @Column(length = ImageConstant.MAX_IMAGE_URL_LENGTH)
+    private String imageUrl;
+
+    public static CommentImage create(Comment comment, String imageUrl){
+        return CommentImage.builder()
+            .comment(comment)
+            .imageUrl(imageUrl)
+            .build();
+    }
+
+    @Builder
+    private CommentImage(Comment comment, String imageUrl) {
+        this.comment = comment;
+        this.imageUrl = imageUrl;
+    }
+}
