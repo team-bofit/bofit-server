a76d1b35ebdab145d9816708b753ef307471da88
jher235 <tim668666@gmail.com>
2025-09-10 22:19:48 +0900

    [Refactor] #136 파라미터를 CommentReplyCreateCommand 로 수정

--- src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyService.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyService.java
@@ -1,10 +1,10 @@
 package org.sopt.bofit.domain.commentreply.service;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.sopt.bofit.domain.comment.entity.Comment;
 import org.sopt.bofit.domain.comment.service.CommentReader;
 import org.sopt.bofit.domain.commentreply.entity.CommentReply;
+import org.sopt.bofit.domain.commentreply.service.dto.request.CommentReplyCreateCommand;
 import org.sopt.bofit.domain.user.entity.User;
 import org.sopt.bofit.domain.user.service.UserReader;
 import org.springframework.stereotype.Service;
@@ -20,14 +20,14 @@ public class CommentReplyService {
     private UserReader userReader;
 
     @Transactional
-    public CommentReply create(Long userId, Long postId, Long commentId, String content, Optional<String> imageUrl){
+    public CommentReply create(Long userId, Long postId, Long commentId, CommentReplyCreateCommand command){
         Comment comment = commentReader.findById(commentId);
         User user = userReader.findById(userId);
 
         comment.checkPost(postId);
 
-        CommentReply commentReply = commentReplyWriter.create(comment, user, content);
-        imageUrl.ifPresent((url) -> commentReplyImageWriter.create(commentReply, url));
+        CommentReply commentReply = commentReplyWriter.create(comment, user, command.content());
+        command.imageUrl().ifPresent((url) -> commentReplyImageWriter.create(commentReply, url));
         return commentReply;
     }
 
