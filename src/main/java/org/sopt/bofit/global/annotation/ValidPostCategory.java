12ced64ff2e33278b4e207666688661d072caae7
dlwjddus1112 <dlwjddus1112@naver.com>
2025-09-12 14:58:13 +0900

    [Chore] #144 게시물 카테고리 유효성 검증 메시지 수정

--- src/main/java/org/sopt/bofit/global/annotation/ValidPostCategory.java
+++ src/main/java/org/sopt/bofit/global/annotation/ValidPostCategory.java
@@ -12,7 +12,7 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = PostCategoryValidator.class)
 @Documented
 public @interface ValidPostCategory {
-    String message() default "게시물 카테고리는 QNA, CONVERSATION, INFORMATION 중에서 선택해주세요.";
+    String message() default "유효한 게시물 카테고리가 아닙니다.";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
