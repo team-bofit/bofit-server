faab7fc2efe53823106bef50d9b9ad53cfa867db
jher235 <tim668666@gmail.com>
2025-09-12 23:21:23 +0900

    [Feat] #147 comment_id를 joincolumn 명시

--- src/main/java/org/sopt/bofit/domain/comment/entity/CommentImage.java
+++ src/main/java/org/sopt/bofit/domain/comment/entity/CommentImage.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -27,6 +28,7 @@ public class CommentImage extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "comment_id")
     private Comment comment;
 
     @Column(length = ImageConstant.MAX_IMAGE_URL_LENGTH)
