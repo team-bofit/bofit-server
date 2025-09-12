90b57f9629ac115ad23c6af89c6883b89c5815c9
jher235 <tim668666@gmail.com>
2025-09-12 22:55:15 +0900

    [Feat] #147 이미지 update 및 delete 를 위한 writer 메서드 작성

--- src/main/java/org/sopt/bofit/domain/comment/service/CommentImageWriter.java
+++ src/main/java/org/sopt/bofit/domain/comment/service/CommentImageWriter.java
@@ -1,9 +1,13 @@
 package org.sopt.bofit.domain.comment.service;
 
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.sopt.bofit.domain.comment.entity.Comment;
 import org.sopt.bofit.domain.comment.entity.CommentImage;
 import org.sopt.bofit.domain.comment.repository.CommentImageRepository;
+import org.sopt.bofit.global.file.dto.request.UpdateImageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +15,32 @@ import org.springframework.stereotype.Service;
 public class CommentImageWriter {
     private final CommentImageRepository commentImageRepository;
 
-    public CommentImage create(Comment comment, String url){
+    public CommentImage create(Comment comment, String url, Integer sequence){
         CommentImage commentImage = CommentImage.create(comment, url);
         return commentImageRepository.save(commentImage);
     }
+
+    @Transactional
+    public void updateAll(
+        Comment comment,
+        Map<Long, CommentImage> currentImages,
+        List<UpdateImageRequest> updatedImages
+    ){
+        updatedImages.forEach(image -> {
+                if(image.id() == null){
+                    create(comment, image.imageUrl(), image.sequence());
+                }else {
+                    currentImages.get(image.id()).updateSequence(image.sequence());
+                }
+            });
+    }
+
+    @Transactional
+    public void softDelete(Map<Long, CommentImage> commentImages, List<Long> deleteImageIds){
+        deleteImageIds.forEach(id -> {
+            commentImages.get(id).softDelete();
+        });
+    }
+
+
 }
