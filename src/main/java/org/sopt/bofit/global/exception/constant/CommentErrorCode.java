d025391ae906d6b3657d820545696873ee4b4f19
jher235 <tim668666@gmail.com>
2025-09-12 22:41:22 +0900

    [Feat] #147 이미지 개수를 초과했을 경우의 예외 추가

--- src/main/java/org/sopt/bofit/global/exception/constant/CommentErrorCode.java
+++ src/main/java/org/sopt/bofit/global/exception/constant/CommentErrorCode.java
@@ -1,21 +1,21 @@
 package org.sopt.bofit.global.exception.constant;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum CommentErrorCode implements ErrorCode{
 
 	UNMATCHED_COMMENT_POST(HttpStatus.BAD_REQUEST.value(), "댓글의 게시글이 일치하지 않습니다"),
+    COMMENT_IMAGE_EXCEED(HttpStatus.BAD_REQUEST.value(), "댓글의 이미지 개수를 초과했습니다."),
 
 	COMMENT_UNAUTHORIZED(HttpStatus.FORBIDDEN.value(), "본인의 댓글만 수정/삭제할 수 있습니다."),
 
 	COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 댓글입니다."),
 
-	COMMENT_ALREADY_DELETED(HttpStatus.CONFLICT.value(), "이미 삭제된 댓글입니다.")
+	COMMENT_ALREADY_DELETED(HttpStatus.CONFLICT.value(), "이미 삭제된 댓글입니다."),
 
 	;
 
