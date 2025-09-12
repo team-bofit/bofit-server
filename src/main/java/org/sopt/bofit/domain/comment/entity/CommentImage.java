8102f6461c4ca1cf76d9b46b3566604be70e2568
jher235 <tim668666@gmail.com>
2025-09-12 22:42:05 +0900

    [Feat] #147 Sequence 필드 추가 - getter 추가 - update, delete 메서드 추가

--- src/main/java/org/sopt/bofit/domain/comment/entity/CommentImage.java
+++ src/main/java/org/sopt/bofit/domain/comment/entity/CommentImage.java
@@ -11,12 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.bofit.global.entity.BaseEntity;
 import org.sopt.bofit.global.file.constant.ImageConstant;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class CommentImage extends BaseEntity {
 
     @Id
@@ -33,6 +35,8 @@ public class CommentImage extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private CommentImageStatus status;
 
+    private Integer sequence;
+
     public static CommentImage create(Comment comment, String imageUrl){
         return CommentImage.builder()
             .comment(comment)
@@ -46,5 +50,14 @@ public class CommentImage extends BaseEntity {
         this.comment = comment;
         this.imageUrl = imageUrl;
         this.status = status;
+    }
+
+    public void softDelete(){
+        this.status = CommentImageStatus.INACTIVE;
+        this.sequence = null;
+    }
+
+    public void updateSequence(Integer newSequence){
+        this.sequence = newSequence;
     }
 }
