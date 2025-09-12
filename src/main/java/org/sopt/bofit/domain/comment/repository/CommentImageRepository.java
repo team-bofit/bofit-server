f4d8f350d4d29dca2f9b0774f234140ec4504632
jher235 <tim668666@gmail.com>
2025-09-12 22:43:01 +0900

    [Feat] #147 Comment 와 status로 CommentImage를 찾는 메서드 추가 - 수정 작업 시 Map 형태가 유용하여 해당 형태로 변환하여 반환하는 default 메서드 추가

--- src/main/java/org/sopt/bofit/domain/comment/repository/CommentImageRepository.java
+++ src/main/java/org/sopt/bofit/domain/comment/repository/CommentImageRepository.java
@@ -1,10 +1,21 @@
 package org.sopt.bofit.domain.comment.repository;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.sopt.bofit.domain.comment.entity.Comment;
 import org.sopt.bofit.domain.comment.entity.CommentImage;
+import org.sopt.bofit.domain.comment.entity.CommentImageStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentImageRepository extends JpaRepository<CommentImage, Long> {
+    List<CommentImage> findAllByCommentAndStatus(Comment comment, CommentImageStatus status);
 
+    default Map<Long, CommentImage> findAllByCommentAndStatusAsMap(Comment comment, CommentImageStatus status){
+        return findAllByCommentAndStatus(comment, status).stream()
+            .collect(Collectors.toMap(CommentImage::getId, Function.identity()));
+    }
 }
