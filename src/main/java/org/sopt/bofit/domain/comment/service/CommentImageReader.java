db431cdcf9d4d9987eda0fe81c2b4a1780cf435d
jher235 <tim668666@gmail.com>
2025-09-12 22:53:06 +0900

    [Feat] #147 CommentImageReader 생성

---
+++ src/main/java/org/sopt/bofit/domain/comment/service/CommentImageReader.java
@@ -0,0 +1,19 @@
+package org.sopt.bofit.domain.comment.service;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.comment.entity.Comment;
+import org.sopt.bofit.domain.comment.entity.CommentImage;
+import org.sopt.bofit.domain.comment.entity.CommentImageStatus;
+import org.sopt.bofit.domain.comment.repository.CommentImageRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentImageReader {
+    private final CommentImageRepository commentImageRepository;
+
+    public Map<Long, CommentImage> getActiveImagesAsMap(Comment comment){
+        return commentImageRepository.findAllByCommentAndStatusAsMap(comment, CommentImageStatus.ACTIVE);
+    }
+}
