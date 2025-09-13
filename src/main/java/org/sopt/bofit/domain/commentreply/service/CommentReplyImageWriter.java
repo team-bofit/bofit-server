b7d3040cb1af2d7cada981712a2fc5b582ba4316
jher235 <tim668666@gmail.com>
2025-09-14 01:28:33 +0900

    [Feat] #152 대댓글 이미지 수정 비즈니스 로직 구현

--- src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyImageWriter.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyImageWriter.java
@@ -1,9 +1,13 @@
 package org.sopt.bofit.domain.commentreply.service;
 
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.sopt.bofit.domain.commentreply.entity.CommentReply;
 import org.sopt.bofit.domain.commentreply.entity.CommentReplyImage;
 import org.sopt.bofit.domain.commentreply.repository.CommentReplyImageRepository;
+import org.sopt.bofit.global.file.dto.request.UpdateImageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +15,31 @@ import org.springframework.stereotype.Service;
 public class CommentReplyImageWriter {
     private final CommentReplyImageRepository commentReplyImageRepository;
 
-    public CommentReplyImage create(CommentReply commentReply, String imageUrl){
-        CommentReplyImage commentReplyImage = CommentReplyImage.create(commentReply, imageUrl);
+    public CommentReplyImage create(CommentReply commentReply, String imageUrl, Integer sequence){
+        CommentReplyImage commentReplyImage = CommentReplyImage.create(commentReply, imageUrl, sequence);
         return commentReplyImageRepository.save(commentReplyImage);
+    }
+
+    @Transactional
+    public void softDelete(Map<Long, CommentReplyImage> commentReplyImages, List<Long> deleteImageIds){
+        deleteImageIds.forEach(id -> {
+            commentReplyImages.get(id).softDelete();
+        });
+    }
+
+    @Transactional
+    public void updateAll(
+        CommentReply commentReply,
+        Map<Long, CommentReplyImage> currentImages,
+        List<UpdateImageRequest> updatedImages
+    ){
+        updatedImages.forEach(image -> {
+            if(image.id() == null){
+                create(commentReply, image.imageUrl(), image.sequence());
+            }else {
+                currentImages.get(image.id()).updateSequence(image.sequence());
+            }
+        });
     }
 
 }
