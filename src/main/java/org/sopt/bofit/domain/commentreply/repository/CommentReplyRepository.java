c7f5d2360826ca2a56b52096da6952131365fc52
jher235 <tim668666@gmail.com>
2025-09-14 01:25:35 +0900

    [Feat] #152 대댓글 조회 로직 구현

--- src/main/java/org/sopt/bofit/domain/commentreply/repository/CommentReplyRepository.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/repository/CommentReplyRepository.java
@@ -1,10 +1,12 @@
 package org.sopt.bofit.domain.commentreply.repository;
 
+import java.util.Optional;
 import org.sopt.bofit.domain.commentreply.entity.CommentReply;
+import org.sopt.bofit.domain.commentreply.entity.CommentReplyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentReplyRepository extends JpaRepository<CommentReply, Long> {
-
+    Optional<CommentReply> findByIdAndStatus(Long id, CommentReplyStatus status);
 }
