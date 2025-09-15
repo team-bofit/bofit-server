ea89e60a793e78d64f35784c20885596f7b66649
dlwjddus1112 <dlwjddus1112@naver.com>
2025-09-15 16:05:06 +0900

    [Chore] #154 이미지 개수 제한 삭제

--- src/main/java/org/sopt/bofit/domain/post/dto/request/PostUpdateRequest.java
+++ src/main/java/org/sopt/bofit/domain/post/dto/request/PostUpdateRequest.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Length;
 import org.sopt.bofit.domain.post.entity.constant.PostInfoConstant;
 import org.sopt.bofit.domain.post.service.dto.request.PostUpdateCommand;
@@ -12,8 +11,6 @@ import org.sopt.bofit.global.annotation.ValidPostCategory;
 import org.sopt.bofit.global.file.dto.request.UpdateImageRequest;
 
 import java.util.List;
-
-import static org.sopt.bofit.domain.comment.constant.CommentConstant.MAX_IMAGE_COUNT;
 
 public record PostUpdateRequest(
         @Schema(description = "제목", example = "ㅇㅇ")
@@ -32,13 +29,11 @@ public record PostUpdateRequest(
         String newCategory,
 
         @Schema(description = "수정된 이미지 목록")
-        @Size(max = MAX_IMAGE_COUNT, message = "이미지 url 의 최대 개수({max}) 를 초과했습니다.")
         @NotNull(message = "수정된 url 목록은  null 일 수 없습니다.")
         @Valid
         List<UpdateImageRequest> updatedImages,
 
         @Schema(description = "삭제할 이미지 ID 목록")
-        @Size(max = MAX_IMAGE_COUNT, message = "이미지 url 의 최대 개수({max}) 를 초과했습니다.")
         @NotNull(message = "삭제된 url 목록은  null 일 수 없습니다.")
         @Valid
         List<@NotNull Long> deleteImageIds
