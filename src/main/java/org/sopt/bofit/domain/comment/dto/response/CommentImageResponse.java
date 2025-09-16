855b52f6fca3337a5fa2115ed2765fede7256f3d
jher235 <tim668666@gmail.com>
2025-09-16 12:14:43 +0900

    [Feat] #156 CommentImage를 반환하기 위한 dto 작성

---
+++ src/main/java/org/sopt/bofit/domain/comment/dto/response/CommentImageResponse.java
@@ -0,0 +1,15 @@
+package org.sopt.bofit.domain.comment.dto.response;
+
+import org.sopt.bofit.domain.comment.entity.CommentImage;
+
+public record CommentImageResponse(
+    Long imageId,
+    String imageUrl,
+    Integer sequence
+) {
+
+    public static CommentImageResponse from(CommentImage commentImage) {
+        return new CommentImageResponse(commentImage.getId(),  commentImage.getImageUrl(), commentImage.getSequence());
+    }
+
+}
