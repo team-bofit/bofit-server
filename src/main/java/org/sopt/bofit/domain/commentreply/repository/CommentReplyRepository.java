5410174bc06e2351e53cc9a5624c5bf364f922cf
jher235 <tim668666@gmail.com>
2025-09-17 17:59:51 +0900

    [Feat] #160 커서 기반 대댓글 목록 조회 메서드 구현

--- src/main/java/org/sopt/bofit/domain/commentreply/repository/CommentReplyRepository.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/repository/CommentReplyRepository.java
@@ -1,8 +1,6 @@
 package org.sopt.bofit.domain.commentreply.repository;
 
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentReplyRepository extends CommentReplyJpaRepository, CommentReplyCustomRepository {
 
 }
