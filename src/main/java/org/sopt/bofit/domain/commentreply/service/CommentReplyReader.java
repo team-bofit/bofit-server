13ca177ba0e5c69d39310b63546e13a47588ab51
jher235 <tim668666@gmail.com>
2025-09-17 18:02:48 +0900

    [Feat] #160 대댓글 목록 조회 로직 작성

--- src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyReader.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyReader.java
@@ -1,11 +1,15 @@
 package org.sopt.bofit.domain.commentreply.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.comment.entity.Comment;
 import org.sopt.bofit.domain.commentreply.entity.CommentReply;
 import org.sopt.bofit.domain.commentreply.entity.CommentReplyStatus;
 import org.sopt.bofit.domain.commentreply.repository.CommentReplyRepository;
+import org.sopt.bofit.domain.commentreply.service.dto.response.CommentReplyResponse;
 import org.sopt.bofit.global.exception.constant.CommentReplyErrorCode;
 import org.sopt.bofit.global.exception.customexception.NotFoundException;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +21,10 @@ public class CommentReplyReader {
     public CommentReply getActiveById(Long commentReplyId) {
         return commentReplyRepository.findByIdAndStatus(commentReplyId, CommentReplyStatus.ACTIVE)
             .orElseThrow(() -> new NotFoundException(CommentReplyErrorCode.COMMENT_REPLY_NOT_FOUND));
+    }
+
+    public Slice<CommentReplyResponse> getAllActiveCommentReply(Comment comment, Optional<Long> cursor, int size){
+        return commentReplyRepository.findActivesByComment(comment, cursor, size);
     }
 
 }
