1696593aef6c9159f12fe858e18c956481157a34
dlwjddus1112 <dlwjddus1112@naver.com>
2025-09-18 18:53:38 +0900

    [Feat] #139 카카오 서버에 탈퇴 요청 보내는 메서드 구현

--- src/main/java/org/sopt/bofit/global/oauth/util/OAuthClient.java
+++ src/main/java/org/sopt/bofit/global/oauth/util/OAuthClient.java
@@ -8,12 +8,16 @@ import org.sopt.bofit.global.oauth.dto.response.KaKaoTokenResponse;
 import org.sopt.bofit.global.oauth.dto.response.KakaoUserResponse;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.sopt.bofit.global.exception.constant.OAuthErrorCode.KAKAO_TOKEN_REQUEST_FAILED;
+import static org.sopt.bofit.global.exception.constant.OAuthErrorCode.UNLINKED_FAILED;
 
 @Component
 @RequiredArgsConstructor
@@ -36,7 +40,7 @@ public class OAuthClient {
                         (req, res) -> {
                             String errorBody = new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8);
                             log.error("❌ Kakao 토큰 요청 실패: {}", errorBody);
-                            throw new BadRequestException(KAKAO_TOKEN_REQUEST_FAILED);
+                            throw new BadRequestException(KAKAO_TOKEN_REQUEST_FAILED, errorBody);
                         })
                 .body(KaKaoTokenResponse.class);
     }
@@ -49,4 +53,27 @@ public class OAuthClient {
                 .body(KakaoUserResponse.class);
     }
 
+    public void unlinkByAdminKey(String kakaoUserId) {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("target_id_type", "user_id");
+        body.add("target_id", kakaoUserId);
+
+        URI uri = OAuthUtil.buildKakaoUnlinkUri(properties.unlinkUri());
+
+        restClient.post()
+                .uri(uri)
+                .header("Authorization", "KakaoAK " + properties.adminKey())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(body)
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        (req, res) -> {
+                            String errorBody = new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8);
+                            log.error(" Kakao 연결 해제 실패 : {} ", errorBody);
+                            throw new BadRequestException(UNLINKED_FAILED, errorBody);
+                        }
+                )
+                .toBodilessEntity();
+    }
 }
