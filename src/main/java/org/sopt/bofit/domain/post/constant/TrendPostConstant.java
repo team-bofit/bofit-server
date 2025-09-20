fe331dd83542abe8a68c6ce0d6dbbe95bb83e8bf
jher235 <tim668666@gmail.com>
2025-09-20 15:03:14 +0900

    [Feat] #164 인기 게시글 관련 상수 클래스 작성

---
+++ src/main/java/org/sopt/bofit/domain/post/constant/TrendPostConstant.java
@@ -0,0 +1,19 @@
+package org.sopt.bofit.domain.post.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrendPostConstant {
+
+    public static final String TREND_POST_DEFAULT_SIZE = "5";
+    public final static String TREND_POST_DEFAULT_SORT = "rank";
+
+    public static final int TREND_POST_SCORED_DATE_RANGE = 7;
+
+    public static final int TREND_POST_CALCULATE_SIZE = 1;
+
+    public static final int TREND_POST_COMMENT_WEIGHT = 1;
+    public static final int TREND_POST_LIKE_WEIGHT = 1;
+
+}
