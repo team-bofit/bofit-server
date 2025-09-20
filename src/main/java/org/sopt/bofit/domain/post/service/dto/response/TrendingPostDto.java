7f85bd49a2651437a3eae91cb2c6295fdc66b2ad
jher235 <tim668666@gmail.com>
2025-09-20 21:32:18 +0900

    [Feat] #164 TrendingPost 를 관리하기 위한 Dto 작성 - 영속성 컨텍스에서 관리하지 않기 위해 기존 엔티티를 사용하던 구조에서 Dto로 분리

---
+++ src/main/java/org/sopt/bofit/domain/post/service/dto/response/TrendingPostDto.java
@@ -0,0 +1,80 @@
+package org.sopt.bofit.domain.post.service.dto.response;
+
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import org.sopt.bofit.domain.post.entity.Post;
+import org.sopt.bofit.domain.post.entity.constant.PostCategory;
+import org.sopt.bofit.domain.post.entity.constant.PostStatus;
+
+@Getter
+public class TrendingPostDto {
+
+    private Long postId;
+
+    private Long writerId;
+    private String writerNickname;
+
+    private String title;
+    private String content;
+    private PostStatus status;
+    private PostCategory postCategory;
+
+    private Long likeCount;
+    private int commentCount;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private TrendingPostDto(Long postId,
+        Long writerId, String writerNickname,
+        String title, String content, PostStatus status, PostCategory postCategory,
+        Long likeCount, int commentCount,
+        LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.postId = postId;
+        this.writerId = writerId;
+        this.writerNickname = writerNickname;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+        this.postCategory = postCategory;
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static TrendingPostDto from(Post post){
+        return TrendingPostDto.builder()
+            .postId(post.getId())
+            .writerId(post.getUser().getId())
+            .writerNickname(post.getWriterNickname())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .status(post.getStatus())
+            .postCategory(post.getPostCategory())
+            .likeCount(post.getLikeCount())
+            .commentCount(post.getCommentCount())
+            .createdAt(post.getCreatedAt())
+            .updatedAt(post.getUpdatedAt())
+            .build();
+    }
+
+    public void increaseLikeCount(){
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount(){
+        this.likeCount--;
+    }
+
+    public void increaseCommentCount(){
+        this.commentCount++;
+    }
+
+    public void decreaseCommentCount(){
+        this.commentCount--;
+    }
+}
