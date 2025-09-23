ec79f9703e8819c6fb25c975dec45574a4d163c5
jher235 <tim668666@gmail.com>
2025-09-23 17:16:13 +0900

    [Feat] #177 CommentReply 관련 이미지 전체 삭제 구현

--- src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyImageWriter.java
+++ src/main/java/org/sopt/bofit/domain/commentreply/service/CommentReplyImageWriter.java
@@ -1,15 +1,15 @@
 package org.sopt.bofit.domain.commentreply.service;
 
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.sopt.bofit.domain.commentreply.entity.CommentReply;
 import org.sopt.bofit.domain.commentreply.entity.CommentReplyImage;
+import org.sopt.bofit.domain.commentreply.entity.CommentReplyImageStatus;
 import org.sopt.bofit.domain.commentreply.repository.CommentReplyImageRepository;
 import org.sopt.bofit.global.file.dto.request.UpdateImageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -41,6 +41,13 @@ public class CommentReplyImageWriter {
                 currentImages.get(image.id()).updateSequence(image.sequence());
             }
         });
+    }
+
+    public void deleteFromCommentReply(CommentReply commentReply){
+        List<CommentReplyImage> activeImages = commentReplyImageRepository.findAllByCommentReplyAndStatus(
+            commentReply, CommentReplyImageStatus.ACTIVE);
+
+        activeImages.forEach(CommentReplyImage::softDelete);
     }
 
 }
