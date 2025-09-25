5cf6c3d6731ac21c55c0c803ee17717694ba0519
jher235 <tim668666@gmail.com>
2025-09-25 13:51:56 +0900

    [Feat] #183 보험 추천 시 선택 항목 조회 로직 추가

---
+++ src/main/java/org/sopt/bofit/domain/insurance/service/InsuranceService.java
@@ -0,0 +1,29 @@
+package org.sopt.bofit.domain.insurance.service;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.sopt.bofit.domain.insurance.dto.response.InsuranceOptionsResponse;
+import org.sopt.bofit.domain.insurance.dto.response.MaturityAgeResponse;
+import org.sopt.bofit.domain.insurance.dto.response.PaymentPeriodResponse;
+import org.sopt.bofit.domain.insurance.dto.response.RefundTypeResponse;
+import org.sopt.bofit.domain.insurance.dto.response.RenewableTypeResponse;
+import org.sopt.bofit.domain.insurance.entity.product.constant.MaturityAge;
+import org.sopt.bofit.domain.insurance.entity.product.constant.PaymentPeriod;
+import org.sopt.bofit.domain.insurance.entity.product.constant.RefundType;
+import org.sopt.bofit.domain.insurance.entity.product.constant.RenewableType;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InsuranceService {
+
+    public InsuranceOptionsResponse getOptional(){
+        return new InsuranceOptionsResponse(
+            Arrays.stream(RenewableType.values()).map(RenewableTypeResponse::create).toList(),
+            Arrays.stream(RefundType.values()).map(RefundTypeResponse::create).toList(),
+            Arrays.stream(PaymentPeriod.values()).map(PaymentPeriodResponse::create).toList(),
+            Arrays.stream(MaturityAge.values()).map(MaturityAgeResponse::create).toList()
+        );
+    }
+
+}
