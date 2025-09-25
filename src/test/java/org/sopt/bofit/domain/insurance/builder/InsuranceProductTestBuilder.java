09593ce5b629c45f46ba12b03495063e82041249
jher235 <tim668666@gmail.com>
2025-09-25 14:17:44 +0900

    [Test] #183 추가된 선택 항목에 맞춰 수정

--- src/test/java/org/sopt/bofit/domain/insurance/builder/InsuranceProductTestBuilder.java
+++ src/test/java/org/sopt/bofit/domain/insurance/builder/InsuranceProductTestBuilder.java
@@ -4,6 +4,10 @@ import org.sopt.bofit.domain.insurance.entity.product.BasicInformation;
 import org.sopt.bofit.domain.insurance.entity.product.ExtraInformation;
 import org.sopt.bofit.domain.insurance.entity.product.InsuranceProduct;
 import org.sopt.bofit.domain.insurance.entity.product.constant.InsuranceStatus;
+import org.sopt.bofit.domain.insurance.entity.product.constant.MaturityAge;
+import org.sopt.bofit.domain.insurance.entity.product.constant.PaymentPeriod;
+import org.sopt.bofit.domain.insurance.entity.product.constant.RefundType;
+import org.sopt.bofit.domain.insurance.entity.product.constant.RenewableType;
 
 public class InsuranceProductTestBuilder extends
 	InsuranceBenefitTestBuilder<InsuranceProductTestBuilder, InsuranceProduct> {
@@ -11,10 +15,12 @@ public class InsuranceProductTestBuilder extends
 	private String name = "테스트용 상품명";
 	private String company = "테스트용 회사명";
 	private int premium = 10000;
-	private int maturityAge = 100;
+	private MaturityAge maturityAge = MaturityAge.OLD_100;
 	private int minEnrollmentAge = 0;
 	private int maxEnrollmentAge = 100;
-	private int paymentPeriodYears = 20;
+	private PaymentPeriod paymentPeriodYears = PaymentPeriod.YEAR_20;
+    private RenewableType renewableType = RenewableType.RENEWABLE;
+    private RefundType refundType = RefundType.FULL_RETURN;
 
 	private String remarks = "테스트용 비고";
 	private String externalUri = "테스용 uri";
@@ -25,10 +31,10 @@ public class InsuranceProductTestBuilder extends
 	public InsuranceProductTestBuilder withName(String name) { this.name = name; return this; }
 	public InsuranceProductTestBuilder withCompany(String company) { this.company = company; return this; }
 	public InsuranceProductTestBuilder withPremium(int premium) { this.premium = premium; return this; }
-	public InsuranceProductTestBuilder withMaturityAge(int age) { this.maturityAge = age; return this; }
+	public InsuranceProductTestBuilder withMaturityAge(MaturityAge age) { this.maturityAge = age; return this; }
 	public InsuranceProductTestBuilder withMinEnrollmentAge(int age) { this.minEnrollmentAge = age; return this; }
 	public InsuranceProductTestBuilder withMaxEnrollmentAge(int age) { this.maxEnrollmentAge = age; return this; }
-	public InsuranceProductTestBuilder withPaymentPeriodYears(int years) { this.paymentPeriodYears = years; return this; }
+	public InsuranceProductTestBuilder withPaymentPeriodYears(PaymentPeriod years) { this.paymentPeriodYears = years; return this; }
 	public InsuranceProductTestBuilder withRemarks(String remarks) { this.remarks = remarks; return this; }
 
 	public InsuranceProductTestBuilder withStatus(InsuranceStatus status) { this.status = status; return this; }
@@ -43,7 +49,9 @@ public class InsuranceProductTestBuilder extends
 			.maturityAge(maturityAge)
 			.minEnrollmentAge(minEnrollmentAge)
 			.maxEnrollmentAge(maxEnrollmentAge)
-			.paymentPeriodYears(paymentPeriodYears)
+            .paymentPeriod(paymentPeriodYears)
+            .refundType(refundType)
+            .renewableType(renewableType)
 			.build();
 
 		ExtraInformation extraInformation = ExtraInformation.builder()
