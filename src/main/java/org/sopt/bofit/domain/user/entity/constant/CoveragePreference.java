5be5344377581cb4a37f32497bbe05aa4b3f8258
jher235 <tim668666@gmail.com>
2025-09-30 00:51:37 +0900

    [Chore] #190 CoveragePreference description 수정 - 기획 요구사항에 맞춰 기존에 제공하던 텍스트를 축약한 형태로 decription 을 수정함

--- src/main/java/org/sopt/bofit/domain/user/entity/constant/CoveragePreference.java
+++ src/main/java/org/sopt/bofit/domain/user/entity/constant/CoveragePreference.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CoveragePreference {
     MAJOR_DISEASE("암, 심장질환같은 큰 병에 대비하고 싶어요", "중대 질병에 대비하고 싶어요"),
-    DEATH_BENEFIT("사망 시 가족에게 경제적 도움이 되는 보장을 원해요", null),
-    ESSENTIAL_ONLY("합리적인 보험료로 꼭 필요한 보장만 챙기고 싶어요", null),
+    DEATH_BENEFIT("사망 시 가족에게 경제적 도움이 되기를 원해요", "사망 시 가족에게 경제적 도움이 되는 보장을 원해요"),
+    ESSENTIAL_ONLY("꼭 필요한 합리적인 보장만 챙기고 싶어요", "합리적인 보험료로 꼭 필요한 보장만 챙기고 싶어요"),
     ACCIDENT_PREDICTION("예기치 않은 사고에 대비하고 싶어요", null),
     SURGERY_COVERAGE("수술할 일이 생겼을 때 보장받고 싶어요", null),
     MAXIMUM_COVERAGE("가격이 좀 높아도 폭넓은 보장을 받고 싶어요", null),
-    RECOMMENDED_OPTION("잘 모르겠어요. 많이 선택하는 걸로 설정할래요", null)
+    RECOMMENDED_OPTION("잘 모르겠어요. 많이 선택하는 걸로 할래요", null)
     ;
 
     private final String description;
