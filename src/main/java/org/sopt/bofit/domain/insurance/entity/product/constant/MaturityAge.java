c0b712fa971d2b6ed4005a7baa71bf2a26e3bb3b
jher235 <tim668666@gmail.com>
2025-10-10 10:43:26 +0900

    [Feat] #204 @JsonValue 로 응답값 변경 - toMaturityAge의 메서드 방식 변경. 시간복잡도 때문에 switch를 썼는데 이런 상황에선 차이가 없고 확장성만 저해시킨다고 판단함.

--- src/main/java/org/sopt/bofit/domain/insurance/entity/product/constant/MaturityAge.java
+++ src/main/java/org/sopt/bofit/domain/insurance/entity/product/constant/MaturityAge.java
@@ -1,5 +1,7 @@
 package org.sopt.bofit.domain.insurance.entity.product.constant;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,17 +11,18 @@ import lombok.Getter;
 public enum MaturityAge {
 
     OLD_80(80, "80세"),
+    OLD_90(90, "90세"),
     OLD_100(100, "100세")
     ;
 
+    @JsonValue
     private final int age;
     private final String displayName;
 
     public static MaturityAge toMaturityAge(int age) {
-        switch (age){
-            case 80: return OLD_80;
-            case 100: return OLD_100;
-            default: return null;
-        }
+        return Arrays.stream(MaturityAge.values())
+            .filter(maturityAge -> maturityAge.getAge() == age)
+            .findFirst()
+            .orElse(null);
     }
 }
