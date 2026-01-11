529e22ed62fdb8559083d28f4cabd37841bd2da0
jher235 <tim668666@gmail.com>
2026-01-12 03:01:11 +0900

    [Refactor] #212 maxTokens json 칼럼명 변경 - max_tokens -> max_completion_tokens. GPT 5부턴 max_completion_tokens 만 지원하며 표준적으로 사용하므로 변경.(정확히는 추론 모델은 이것만 지원함) - reasoningEffort, responseFormat 추가

--- src/main/java/org/sopt/bofit/global/external/generativeai/openai/dto/request/OpenAiRequest.java
+++ src/main/java/org/sopt/bofit/global/external/generativeai/openai/dto/request/OpenAiRequest.java
@@ -6,13 +6,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+/**
+ * reasoningEffort: 추론 정도 조절. minimal, low, medium, high
+ */
 @Getter
 @AllArgsConstructor
 @ToString
 public class OpenAiRequest {
 	private String model;
 	private List<ChatRequestMessage> messages;
-	@JsonProperty("max_tokens")
+	@JsonProperty("max_completion_tokens")
 	private int maxTokens;
 	private double temperature;
+    @JsonProperty("response_format")
+    private ResponseFormat responseFormat;
+    @JsonProperty("reasoning_effort")
+    private String reasoningEffort;
 }
